ea10f8610901cde1571d76e3ffa2700b97349a5c
arseniy-cage <lllufoxnewyear2023@gmail.com>
2025-12-15 17:20:14 +1000

    JNI build 2025-12-15 17:20:14

---
+++ jni/jni/obj/local/armeabi-v7a/objs/plugin/vendor/Substrate/hde64.o.d
@@ -0,0 +1,16 @@
+./obj/local/armeabi-v7a/objs/plugin/vendor/Substrate/hde64.o: \
+  jni/vendor/Substrate/hde64.c \
+  C:/ndk25/build/../sources/cxx-stl/llvm-libc++/include/stdint.h \
+  C:/ndk25/build/../sources/cxx-stl/llvm-libc++/include/__config \
+  C:/ndk25/build/../sources/cxx-stl/llvm-libc++/include/string.h \
+  jni/vendor/Substrate/hde64.h jni/vendor/Substrate/table64.h
+
+C:/ndk25/build/../sources/cxx-stl/llvm-libc++/include/stdint.h:
+
+C:/ndk25/build/../sources/cxx-stl/llvm-libc++/include/__config:
+
+C:/ndk25/build/../sources/cxx-stl/llvm-libc++/include/string.h:
+
+jni/vendor/Substrate/hde64.h:
+
+jni/vendor/Substrate/table64.h:

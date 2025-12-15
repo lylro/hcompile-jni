8770b071fe22f5cb1d41237e6e921e6282d75e1d
arseniy-cage <lllufoxnewyear2023@gmail.com>
2025-12-15 18:27:08 +1000

    JNI build 2025-12-15 18:27:08

---
+++ jni/obj/local/armeabi-v7a/objs/plugin/vendor/RakNet/Rand.o.d
@@ -0,0 +1,11 @@
+./obj/local/armeabi-v7a/objs/plugin/vendor/RakNet/Rand.o: \
+  jni/vendor/RakNet/Rand.cpp \
+  C:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/stdio.h \
+  C:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/__config \
+  C:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/stdlib.h
+
+C:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/stdio.h:
+
+C:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/__config:
+
+C:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/stdlib.h:

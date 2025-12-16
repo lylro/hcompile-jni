34c877c1afddd832195f746732f397bd3b7efeac
arseniy-cage <lllufoxnewyear2023@gmail.com>
2025-12-17 01:51:21 +1000

    Clean repository 2025-12-17 01:51:21

--- jni/jnii/obj/local/armeabi-v7a/objs/NARKOTA/network/quaternion.o.d
+++
@@ -1,7 +0,0 @@
-./obj/local/armeabi-v7a/objs/NARKOTA/network/quaternion.o: \
-  jni/network/quaternion.cpp jni/network/quaternion.h \
-  C:/ndk-r16b/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\string
-
-jni/network/quaternion.h:
-
-C:/ndk-r16b/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\string:
--- jni/jnii/obj/local/armeabi-v7a/objs/NARKOTA/vendor/raknet/DS_ByteQueue.o.d
+++
@@ -1,7 +0,0 @@
-./obj/local/armeabi-v7a/objs/NARKOTA/vendor/raknet/DS_ByteQueue.o: \
-  jni/vendor/raknet/DS_ByteQueue.cpp jni/vendor/raknet/DS_ByteQueue.h \
-  jni/vendor/raknet/Export.h
-
-jni/vendor/raknet/DS_ByteQueue.h:
-
-jni/vendor/raknet/Export.h:

5d8f09261e3ad270bb0cf9f696df4162d9ffd90d
arseniy-cage <lllufoxnewyear2023@gmail.com>
2025-12-17 03:18:34 +1000

    JNI build 2025-12-17 03:18:34

---
+++ jni/gui jni by tgkshram/obj/local/armeabi-v7a/objs/plugin/game/chat.o.d
@@ -0,0 +1,162 @@
+./obj/local/armeabi-v7a/objs/plugin/game/chat.o: jni/game/chat.cpp \
+  jni/game/chat.h \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cstdarg \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__config \
+  jni/xorstr.h jni/plugin.h \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cstdint \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stdint.h \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/string.h \
+  jni/offsets.h \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/vector \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/iosfwd \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/wchar.h \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__bit_reference \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/bit \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/limits \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/type_traits \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cstddef \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/version \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__debug \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/algorithm \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/initializer_list \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cstring \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/utility \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__tuple \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/memory \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/typeinfo \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/exception \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cstdlib \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stdlib.h \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/new \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/iterator \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__functional_base \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/tuple \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stdexcept \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/atomic \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__threading_support \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/chrono \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/ctime \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/ratio \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/climits \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/limits.h \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/errno.h \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/functional \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__split_buffer \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/string \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/string_view \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__string \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cstdio \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stdio.h \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cwchar \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cwctype \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cctype \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/ctype.h \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/wctype.h
+
+jni/game/chat.h:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cstdarg:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__config:
+
+jni/xorstr.h:
+
+jni/plugin.h:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cstdint:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stdint.h:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/string.h:
+
+jni/offsets.h:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/vector:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/iosfwd:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/wchar.h:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__bit_reference:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/bit:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/limits:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/type_traits:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cstddef:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/version:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__debug:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/algorithm:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/initializer_list:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cstring:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/utility:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__tuple:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/memory:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/typeinfo:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/exception:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cstdlib:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stdlib.h:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/new:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/iterator:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__functional_base:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/tuple:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stdexcept:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/atomic:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__threading_support:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/chrono:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/ctime:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/ratio:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/climits:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/limits.h:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/errno.h:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/functional:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__split_buffer:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/string:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/string_view:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__string:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cstdio:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stdio.h:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cwchar:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cwctype:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cctype:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/ctype.h:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/wctype.h:

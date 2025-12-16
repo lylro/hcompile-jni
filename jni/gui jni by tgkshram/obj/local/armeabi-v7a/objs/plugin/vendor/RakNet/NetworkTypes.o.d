dec3b741706d686377a616b271d6a0afbbc8e7d4
arseniy-cage <lllufoxnewyear2023@gmail.com>
2025-12-17 03:22:31 +1000

    JNI build 2025-12-17 03:22:31

---
+++ jni/gui jni by tgkshram/obj/local/armeabi-v7a/objs/plugin/vendor/RakNet/NetworkTypes.o.d
@@ -0,0 +1,18 @@
+./obj/local/armeabi-v7a/objs/plugin/vendor/RakNet/NetworkTypes.o: \
+  jni/vendor/RakNet/NetworkTypes.cpp jni/vendor/RakNet/NetworkTypes.h \
+  jni/vendor/RakNet/RakNetDefines.h jni/vendor/RakNet/Export.h \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/string.h \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__config \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stdio.h
+
+jni/vendor/RakNet/NetworkTypes.h:
+
+jni/vendor/RakNet/RakNetDefines.h:
+
+jni/vendor/RakNet/Export.h:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/string.h:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__config:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stdio.h:

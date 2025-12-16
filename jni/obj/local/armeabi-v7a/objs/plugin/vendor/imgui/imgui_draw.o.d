7dff12877779083bda315e91ff0dba4c79f80794
arseniy-cage <lllufoxnewyear2023@gmail.com>
2025-12-16 20:10:32 +1000

    JNI build 2025-12-16 20:10:32

---
+++ jni/obj/local/armeabi-v7a/objs/plugin/vendor/imgui/imgui_draw.o.d
@@ -0,0 +1,39 @@
+./obj/local/armeabi-v7a/objs/plugin/vendor/imgui/imgui_draw.o: \
+  jni/vendor/imgui/imgui_draw.cpp jni/vendor/imgui/imgui.h \
+  jni/vendor/imgui/imconfig.h \
+  C:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/float.h \
+  C:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/__config \
+  C:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/stddef.h \
+  C:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/string.h \
+  jni/vendor/imgui/imgui_internal.h \
+  C:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/stdio.h \
+  C:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/stdlib.h \
+  C:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/math.h \
+  C:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/limits.h \
+  jni/vendor/imgui/imstb_rectpack.h jni/vendor/imgui/imstb_truetype.h
+
+jni/vendor/imgui/imgui.h:
+
+jni/vendor/imgui/imconfig.h:
+
+C:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/float.h:
+
+C:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/__config:
+
+C:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/stddef.h:
+
+C:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/string.h:
+
+jni/vendor/imgui/imgui_internal.h:
+
+C:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/stdio.h:
+
+C:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/stdlib.h:
+
+C:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/math.h:
+
+C:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/limits.h:
+
+jni/vendor/imgui/imstb_rectpack.h:
+
+jni/vendor/imgui/imstb_truetype.h:

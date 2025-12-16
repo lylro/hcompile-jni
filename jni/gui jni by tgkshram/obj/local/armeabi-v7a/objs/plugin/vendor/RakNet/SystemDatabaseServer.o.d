c8d4968e2ba84f6fe42c70e196b9ebd654a9c011
arseniy-cage <lllufoxnewyear2023@gmail.com>
2025-12-17 03:28:46 +1000

    JNI build 2025-12-17 03:28:45

---
+++ jni/gui jni by tgkshram/obj/local/armeabi-v7a/objs/plugin/vendor/RakNet/SystemDatabaseServer.o.d
@@ -0,0 +1,5 @@
+./obj/local/armeabi-v7a/objs/plugin/vendor/RakNet/SystemDatabaseServer.o: \
+  jni/vendor/RakNet/SystemDatabaseServer.cpp \
+  jni/vendor/RakNet/SystemDatabaseServer.h
+
+jni/vendor/RakNet/SystemDatabaseServer.h:

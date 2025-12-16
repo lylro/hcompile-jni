c9cff00e90c786a64255e6499de67174a71ac216
arseniy-cage <lllufoxnewyear2023@gmail.com>
2025-12-16 23:08:32 +1000

    Clean repository 2025-12-16 23:08:32

--- jni/obj/local/arm64-v8a/objs/plugin/vendor/RakNet/SystemDatabaseServer.o.d
+++
@@ -1,5 +0,0 @@
-./obj/local/arm64-v8a/objs/plugin/vendor/RakNet/SystemDatabaseServer.o: \
-  jni\vendor\RakNet\SystemDatabaseServer.cpp \
-  jni\vendor\RakNet\SystemDatabaseServer.h
-
-jni\vendor\RakNet\SystemDatabaseServer.h:

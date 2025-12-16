a1a991704862e6a577a37a05a7de0582523042c2
arseniy-cage <lllufoxnewyear2023@gmail.com>
2025-12-16 23:47:30 +1000

    Clean repository 2025-12-16 23:47:30

--- jni/obj/local/arm64-v8a/objs/plugin/vendor/RakNet/BitStream_NoTemplate.o.d
+++
@@ -1,2 +0,0 @@
-./obj/local/arm64-v8a/objs/plugin/vendor/RakNet/BitStream_NoTemplate.o: \
-  jni\vendor\RakNet\BitStream_NoTemplate.cpp
--- jni/obj/local/arm64-v8a/objs/plugin/vendor/RakNet/RakSleep.o.d
+++
@@ -1,2 +0,0 @@
-./obj/local/arm64-v8a/objs/plugin/vendor/RakNet/RakSleep.o: \
-  jni\vendor\RakNet\RakSleep.cpp

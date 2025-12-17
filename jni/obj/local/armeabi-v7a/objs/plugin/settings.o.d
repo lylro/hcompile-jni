8a6b9bdf545d6bba001028c77e534601513723e3
arseniy-cage <lllufoxnewyear2023@gmail.com>
2025-12-17 23:15:06 +1000

    JNI build 2025-12-17 23:15:06

---
+++ jni/obj/local/armeabi-v7a/objs/plugin/settings.o.d
@@ -0,0 +1,31 @@
+./obj/local/armeabi-v7a/objs/plugin/settings.o: jni\settings.cpp \
+  jni\config.h \
+  C:\Users\Unibest\Desktop\LAIRDPROJECT\android-ndk-r21e\build\\..\sources\cxx-stl\llvm-libc++\include\stdbool.h \
+  C:\Users\Unibest\Desktop\LAIRDPROJECT\android-ndk-r21e\build\\..\sources\cxx-stl\llvm-libc++\include\__config \
+  jni\settings.h \
+  C:\Users\Unibest\Desktop\LAIRDPROJECT\android-ndk-r21e\build\\..\sources\cxx-stl\llvm-libc++\include\cstring \
+  C:\Users\Unibest\Desktop\LAIRDPROJECT\android-ndk-r21e\build\\..\sources\cxx-stl\llvm-libc++\include\string.h \
+  C:\Users\Unibest\Desktop\LAIRDPROJECT\android-ndk-r21e\build\\..\sources\cxx-stl\llvm-libc++\include\cstdio \
+  C:\Users\Unibest\Desktop\LAIRDPROJECT\android-ndk-r21e\build\\..\sources\cxx-stl\llvm-libc++\include\stdio.h \
+  C:\Users\Unibest\Desktop\LAIRDPROJECT\android-ndk-r21e\build\\..\sources\cxx-stl\llvm-libc++\include\cstdlib \
+  C:\Users\Unibest\Desktop\LAIRDPROJECT\android-ndk-r21e\build\\..\sources\cxx-stl\llvm-libc++\include\stdlib.h
+
+jni\config.h:
+
+C:\Users\Unibest\Desktop\LAIRDPROJECT\android-ndk-r21e\build\\..\sources\cxx-stl\llvm-libc++\include\stdbool.h:
+
+C:\Users\Unibest\Desktop\LAIRDPROJECT\android-ndk-r21e\build\\..\sources\cxx-stl\llvm-libc++\include\__config:
+
+jni\settings.h:
+
+C:\Users\Unibest\Desktop\LAIRDPROJECT\android-ndk-r21e\build\\..\sources\cxx-stl\llvm-libc++\include\cstring:
+
+C:\Users\Unibest\Desktop\LAIRDPROJECT\android-ndk-r21e\build\\..\sources\cxx-stl\llvm-libc++\include\string.h:
+
+C:\Users\Unibest\Desktop\LAIRDPROJECT\android-ndk-r21e\build\\..\sources\cxx-stl\llvm-libc++\include\cstdio:
+
+C:\Users\Unibest\Desktop\LAIRDPROJECT\android-ndk-r21e\build\\..\sources\cxx-stl\llvm-libc++\include\stdio.h:
+
+C:\Users\Unibest\Desktop\LAIRDPROJECT\android-ndk-r21e\build\\..\sources\cxx-stl\llvm-libc++\include\cstdlib:
+
+C:\Users\Unibest\Desktop\LAIRDPROJECT\android-ndk-r21e\build\\..\sources\cxx-stl\llvm-libc++\include\stdlib.h:
---
+++ jni/obj/local/armeabi-v7a/objs/plugin/vendor/RakNet/Rand.o.d
@@ -0,0 +1,11 @@
+./obj/local/armeabi-v7a/objs/plugin/vendor/RakNet/Rand.o: \
+  jni\vendor\RakNet\Rand.cpp \
+  C:\Users\Unibest\Desktop\LAIRDPROJECT\android-ndk-r21e\build\\..\sources\cxx-stl\llvm-libc++\include\stdio.h \
+  C:\Users\Unibest\Desktop\LAIRDPROJECT\android-ndk-r21e\build\\..\sources\cxx-stl\llvm-libc++\include\__config \
+  C:\Users\Unibest\Desktop\LAIRDPROJECT\android-ndk-r21e\build\\..\sources\cxx-stl\llvm-libc++\include\stdlib.h
+
+C:\Users\Unibest\Desktop\LAIRDPROJECT\android-ndk-r21e\build\\..\sources\cxx-stl\llvm-libc++\include\stdio.h:
+
+C:\Users\Unibest\Desktop\LAIRDPROJECT\android-ndk-r21e\build\\..\sources\cxx-stl\llvm-libc++\include\__config:
+
+C:\Users\Unibest\Desktop\LAIRDPROJECT\android-ndk-r21e\build\\..\sources\cxx-stl\llvm-libc++\include\stdlib.h:

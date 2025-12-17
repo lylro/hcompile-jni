575c2b15ac1e01fc5b0f984f9bd04adfa4f28920
arseniy-cage <lllufoxnewyear2023@gmail.com>
2025-12-17 22:53:43 +1000

    JNI build 2025-12-17 22:53:43

---
+++ jni/obj/local/armeabi-v7a/objs/plugin/vendor/RakNet/StringTable.o.d
@@ -0,0 +1,40 @@
+./obj/local/armeabi-v7a/objs/plugin/vendor/RakNet/StringTable.o: \
+  jni\vendor\RakNet\StringTable.cpp jni\vendor\RakNet\StringTable.h \
+  jni\vendor\RakNet\DS_OrderedList.h jni\vendor\RakNet\DS_List.h \
+  C:\Users\Unibest\Desktop\LAIRDPROJECT\android-ndk-r21e\build\\..\sources\cxx-stl\llvm-libc++\include\string.h \
+  C:\Users\Unibest\Desktop\LAIRDPROJECT\android-ndk-r21e\build\\..\sources\cxx-stl\llvm-libc++\include\__config \
+  jni\vendor\RakNet\Export.h \
+  C:\Users\Unibest\Desktop\LAIRDPROJECT\android-ndk-r21e\build\\..\sources\cxx-stl\llvm-libc++\include\stdio.h \
+  jni\vendor\RakNet\BitStream.h jni\vendor\RakNet\RakNetDefines.h \
+  jni\vendor\RakNet\NetworkTypes.h \
+  C:\Users\Unibest\Desktop\LAIRDPROJECT\android-ndk-r21e\build\\..\sources\cxx-stl\llvm-libc++\include\math.h \
+  C:\Users\Unibest\Desktop\LAIRDPROJECT\android-ndk-r21e\build\\..\sources\cxx-stl\llvm-libc++\include\float.h \
+  jni\vendor\RakNet\StringCompressor.h jni\vendor\RakNet\DS_Map.h
+
+jni\vendor\RakNet\StringTable.h:
+
+jni\vendor\RakNet\DS_OrderedList.h:
+
+jni\vendor\RakNet\DS_List.h:
+
+C:\Users\Unibest\Desktop\LAIRDPROJECT\android-ndk-r21e\build\\..\sources\cxx-stl\llvm-libc++\include\string.h:
+
+C:\Users\Unibest\Desktop\LAIRDPROJECT\android-ndk-r21e\build\\..\sources\cxx-stl\llvm-libc++\include\__config:
+
+jni\vendor\RakNet\Export.h:
+
+C:\Users\Unibest\Desktop\LAIRDPROJECT\android-ndk-r21e\build\\..\sources\cxx-stl\llvm-libc++\include\stdio.h:
+
+jni\vendor\RakNet\BitStream.h:
+
+jni\vendor\RakNet\RakNetDefines.h:
+
+jni\vendor\RakNet\NetworkTypes.h:
+
+C:\Users\Unibest\Desktop\LAIRDPROJECT\android-ndk-r21e\build\\..\sources\cxx-stl\llvm-libc++\include\math.h:
+
+C:\Users\Unibest\Desktop\LAIRDPROJECT\android-ndk-r21e\build\\..\sources\cxx-stl\llvm-libc++\include\float.h:
+
+jni\vendor\RakNet\StringCompressor.h:
+
+jni\vendor\RakNet\DS_Map.h:

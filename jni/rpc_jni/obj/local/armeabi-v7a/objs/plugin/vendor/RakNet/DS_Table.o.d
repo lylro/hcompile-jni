e0ba5a44c43d2c026a91d4076fbe6ce040bdf800
arseniy-cage <lllufoxnewyear2023@gmail.com>
2025-12-18 01:48:58 +1000

    JNI build 2025-12-18 01:48:58

---
+++ jni/rpc_jni/obj/local/armeabi-v7a/objs/plugin/vendor/RakNet/DS_Table.o.d
@@ -0,0 +1,29 @@
+./obj/local/armeabi-v7a/objs/plugin/vendor/RakNet/DS_Table.o: \
+  jni/vendor/RakNet/DS_Table.cpp jni/vendor/RakNet/DS_Table.h \
+  jni/vendor/RakNet/DS_List.h \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/string.h \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__config \
+  jni/vendor/RakNet/Export.h jni/vendor/RakNet/DS_BPlusTree.h \
+  jni/vendor/RakNet/DS_MemoryPool.h jni/vendor/RakNet/DS_Queue.h \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stdio.h \
+  jni/vendor/RakNet/DS_OrderedList.h
+
+jni/vendor/RakNet/DS_Table.h:
+
+jni/vendor/RakNet/DS_List.h:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/string.h:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__config:
+
+jni/vendor/RakNet/Export.h:
+
+jni/vendor/RakNet/DS_BPlusTree.h:
+
+jni/vendor/RakNet/DS_MemoryPool.h:
+
+jni/vendor/RakNet/DS_Queue.h:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stdio.h:
+
+jni/vendor/RakNet/DS_OrderedList.h:

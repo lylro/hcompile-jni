3568d3fc20b9f1d690aeca9e6de533ef6271357f
arseniy-cage <lllufoxnewyear2023@gmail.com>
2025-12-18 18:57:20 +1000

    JNI build 2025-12-18 18:57:20

---
+++ jni/gui jni by tgkshram/obj/local/armeabi-v7a/objs/plugin/plugin/common.o.d
@@ -0,0 +1,432 @@
+./obj/local/armeabi-v7a/objs/plugin/plugin/common.o: \
+  jni/plugin/common.cpp jni/plugin/common.h \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cstdint \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__config \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stdint.h \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/string \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/string_view \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__string \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/algorithm \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/initializer_list \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cstddef \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/version \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/type_traits \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cstring \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/string.h \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/utility \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__tuple \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__debug \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/iosfwd \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/wchar.h \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/memory \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/typeinfo \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/exception \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cstdlib \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stdlib.h \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/new \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/limits \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/iterator \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__functional_base \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/tuple \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stdexcept \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/atomic \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__threading_support \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/chrono \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/ctime \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/ratio \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/climits \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/limits.h \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/errno.h \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/functional \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/bit \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cstdio \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stdio.h \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cwchar \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cwctype \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cctype \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/ctype.h \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/wctype.h \
+  jni/vendor/RakNet/BitStream.h jni/vendor/RakNet/RakNetDefines.h \
+  jni/vendor/RakNet/Export.h jni/vendor/RakNet/NetworkTypes.h \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/math.h \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/float.h \
+  jni/vendor/RakNet/SAMP/SAMPRPC.h jni/vendor/RakNet/PacketPriority.h \
+  jni/game/math/vector.h \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cmath \
+  jni/plugin/netgame.h jni/vendor/RakNet/RakNetworkFactory.h \
+  jni/vendor/RakNet/RakClient.h jni/vendor/RakNet/RakPeer.h \
+  jni/vendor/RakNet/RakPeerInterface.h \
+  jni/vendor/RakNet/ReliabilityLayer.h jni/vendor/RakNet/SocketLayer.h \
+  jni/vendor/RakNet/ClientContextStruct.h jni/vendor/RakNet/MTUSize.h \
+  jni/vendor/RakNet/DS_LinkedList.h jni/vendor/RakNet/DS_List.h \
+  jni/vendor/RakNet/DS_Queue.h jni/vendor/RakNet/InternalPacket.h \
+  jni/vendor/RakNet/InternalPacketPool.h \
+  jni/vendor/RakNet/DataBlockEncryptor.h jni/vendor/RakNet/rijndael.h \
+  jni/vendor/RakNet/RakNetStatistics.h jni/vendor/RakNet/SHA1.h \
+  jni/vendor/RakNet/Types.h jni/vendor/RakNet/DS_OrderedList.h \
+  jni/vendor/RakNet/DS_RangeList.h jni/vendor/RakNet/DS_BPlusTree.h \
+  jni/vendor/RakNet/DS_MemoryPool.h jni/vendor/RakNet/RPCNode.h \
+  jni/vendor/RakNet/RSACrypt.h jni/vendor/RakNet/BigTypes.h \
+  jni/vendor/RakNet/Rand.h jni/vendor/RakNet/SingleProducerConsumer.h \
+  jni/vendor/RakNet/RPCMap.h jni/vendor/RakNet/SimpleMutex.h \
+  jni/vendor/RakNet/RakClientInterface.h \
+  jni/vendor/RakNet/PacketEnumerations.h jni/game/chat.h \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cstdarg \
+  jni/plugin/pools/playerpool.h jni/vendor/EASTL/array.h \
+  jni/vendor/EASTL/internal/config.h jni/vendor/EABase/eabase.h \
+  jni/vendor/EABase/config/eacompiler.h \
+  jni/vendor/EABase/config/eaplatform.h \
+  jni/vendor/EABase/config/eacompilertraits.h \
+  jni/vendor/EABase/version.h jni/vendor/EABase/eaunits.h \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/inttypes.h \
+  jni/vendor/EABase/int128.h jni/vendor/EABase/eadeprecated.h \
+  jni/vendor/EABase/eahave.h \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stddef.h \
+  jni/vendor/EASTL/internal/tuple_fwd_decls.h \
+  jni/vendor/EASTL/iterator.h jni/vendor/EASTL/internal/move_help.h \
+  jni/vendor/EASTL/type_traits.h \
+  jni/vendor/EASTL/internal/type_fundamental.h \
+  jni/vendor/EABase/nullptr.h \
+  jni/vendor/EASTL/internal/type_transformations.h \
+  jni/vendor/EASTL/internal/type_void_t.h \
+  jni/vendor/EASTL/internal/type_properties.h \
+  jni/vendor/EASTL/internal/type_compound.h \
+  jni/vendor/EASTL/internal/type_pod.h \
+  jni/vendor/EASTL/internal/type_detected.h \
+  jni/vendor/EASTL/internal/memory_base.h \
+  jni/vendor/EASTL/initializer_list.h jni/vendor/EASTL/algorithm.h \
+  jni/vendor/EASTL/internal/copy_help.h \
+  jni/vendor/EASTL/internal/fill_help.h jni/vendor/EASTL/functional.h \
+  jni/vendor/EASTL/internal/functional_base.h \
+  jni/vendor/EASTL/internal/mem_fn.h \
+  jni/vendor/EASTL/internal/function.h \
+  jni/vendor/EASTL/internal/function_detail.h \
+  jni/vendor/EASTL/internal/function_help.h jni/vendor/EASTL/utility.h \
+  jni/vendor/EASTL/numeric_limits.h jni/vendor/EASTL/compare.h \
+  jni/vendor/EASTL/meta.h jni/vendor/EASTL/internal/integer_sequence.h \
+  jni/vendor/EASTL/internal/in_place_t.h \
+  jni/vendor/EASTL/internal/piecewise_construct_t.h \
+  jni/vendor/EASTL/allocator.h jni/vendor/EASTL/random.h \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/array \
+  jni/plugin/pools/../localplayer.h jni/game/CPlayerPed.h \
+  jni/game/CPhysical.h jni/game/CEntryInfo.h jni/game/CEntity.h \
+  jni/game/CPlaceable.h jni/game/math/matrix.h \
+  jni/plugin/pools/../remoteplayer.h jni/vendor/EASTL/unordered_map.h \
+  jni/vendor/EASTL/hash_map.h jni/vendor/EASTL/internal/hashtable.h \
+  jni/vendor/EASTL/tuple.h jni/plugin.h jni/offsets.h \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/vector \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__bit_reference \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__split_buffer \
+  jni/xorstr.h
+
+jni/plugin/common.h:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cstdint:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__config:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stdint.h:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/string:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/string_view:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__string:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/algorithm:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/initializer_list:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cstddef:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/version:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/type_traits:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cstring:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/string.h:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/utility:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__tuple:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__debug:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/iosfwd:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/wchar.h:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/memory:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/typeinfo:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/exception:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cstdlib:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stdlib.h:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/new:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/limits:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/iterator:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__functional_base:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/tuple:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stdexcept:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/atomic:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__threading_support:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/chrono:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/ctime:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/ratio:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/climits:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/limits.h:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/errno.h:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/functional:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/bit:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cstdio:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stdio.h:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cwchar:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cwctype:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cctype:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/ctype.h:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/wctype.h:
+
+jni/vendor/RakNet/BitStream.h:
+
+jni/vendor/RakNet/RakNetDefines.h:
+
+jni/vendor/RakNet/Export.h:
+
+jni/vendor/RakNet/NetworkTypes.h:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/math.h:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/float.h:
+
+jni/vendor/RakNet/SAMP/SAMPRPC.h:
+
+jni/vendor/RakNet/PacketPriority.h:
+
+jni/game/math/vector.h:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cmath:
+
+jni/plugin/netgame.h:
+
+jni/vendor/RakNet/RakNetworkFactory.h:
+
+jni/vendor/RakNet/RakClient.h:
+
+jni/vendor/RakNet/RakPeer.h:
+
+jni/vendor/RakNet/RakPeerInterface.h:
+
+jni/vendor/RakNet/ReliabilityLayer.h:
+
+jni/vendor/RakNet/SocketLayer.h:
+
+jni/vendor/RakNet/ClientContextStruct.h:
+
+jni/vendor/RakNet/MTUSize.h:
+
+jni/vendor/RakNet/DS_LinkedList.h:
+
+jni/vendor/RakNet/DS_List.h:
+
+jni/vendor/RakNet/DS_Queue.h:
+
+jni/vendor/RakNet/InternalPacket.h:
+
+jni/vendor/RakNet/InternalPacketPool.h:
+
+jni/vendor/RakNet/DataBlockEncryptor.h:
+
+jni/vendor/RakNet/rijndael.h:
+
+jni/vendor/RakNet/RakNetStatistics.h:
+
+jni/vendor/RakNet/SHA1.h:
+
+jni/vendor/RakNet/Types.h:
+
+jni/vendor/RakNet/DS_OrderedList.h:
+
+jni/vendor/RakNet/DS_RangeList.h:
+
+jni/vendor/RakNet/DS_BPlusTree.h:
+
+jni/vendor/RakNet/DS_MemoryPool.h:
+
+jni/vendor/RakNet/RPCNode.h:
+
+jni/vendor/RakNet/RSACrypt.h:
+
+jni/vendor/RakNet/BigTypes.h:
+
+jni/vendor/RakNet/Rand.h:
+
+jni/vendor/RakNet/SingleProducerConsumer.h:
+
+jni/vendor/RakNet/RPCMap.h:
+
+jni/vendor/RakNet/SimpleMutex.h:
+
+jni/vendor/RakNet/RakClientInterface.h:
+
+jni/vendor/RakNet/PacketEnumerations.h:
+
+jni/game/chat.h:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cstdarg:
+
+jni/plugin/pools/playerpool.h:
+
+jni/vendor/EASTL/array.h:
+
+jni/vendor/EASTL/internal/config.h:
+
+jni/vendor/EABase/eabase.h:
+
+jni/vendor/EABase/config/eacompiler.h:
+
+jni/vendor/EABase/config/eaplatform.h:
+
+jni/vendor/EABase/config/eacompilertraits.h:
+
+jni/vendor/EABase/version.h:
+
+jni/vendor/EABase/eaunits.h:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/inttypes.h:
+
+jni/vendor/EABase/int128.h:
+
+jni/vendor/EABase/eadeprecated.h:
+
+jni/vendor/EABase/eahave.h:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stddef.h:
+
+jni/vendor/EASTL/internal/tuple_fwd_decls.h:
+
+jni/vendor/EASTL/iterator.h:
+
+jni/vendor/EASTL/internal/move_help.h:
+
+jni/vendor/EASTL/type_traits.h:
+
+jni/vendor/EASTL/internal/type_fundamental.h:
+
+jni/vendor/EABase/nullptr.h:
+
+jni/vendor/EASTL/internal/type_transformations.h:
+
+jni/vendor/EASTL/internal/type_void_t.h:
+
+jni/vendor/EASTL/internal/type_properties.h:
+
+jni/vendor/EASTL/internal/type_compound.h:
+
+jni/vendor/EASTL/internal/type_pod.h:
+
+jni/vendor/EASTL/internal/type_detected.h:
+
+jni/vendor/EASTL/internal/memory_base.h:
+
+jni/vendor/EASTL/initializer_list.h:
+
+jni/vendor/EASTL/algorithm.h:
+
+jni/vendor/EASTL/internal/copy_help.h:
+
+jni/vendor/EASTL/internal/fill_help.h:
+
+jni/vendor/EASTL/functional.h:
+
+jni/vendor/EASTL/internal/functional_base.h:
+
+jni/vendor/EASTL/internal/mem_fn.h:
+
+jni/vendor/EASTL/internal/function.h:
+
+jni/vendor/EASTL/internal/function_detail.h:
+
+jni/vendor/EASTL/internal/function_help.h:
+
+jni/vendor/EASTL/utility.h:
+
+jni/vendor/EASTL/numeric_limits.h:
+
+jni/vendor/EASTL/compare.h:
+
+jni/vendor/EASTL/meta.h:
+
+jni/vendor/EASTL/internal/integer_sequence.h:
+
+jni/vendor/EASTL/internal/in_place_t.h:
+
+jni/vendor/EASTL/internal/piecewise_construct_t.h:
+
+jni/vendor/EASTL/allocator.h:
+
+jni/vendor/EASTL/random.h:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/array:
+
+jni/plugin/pools/../localplayer.h:
+
+jni/game/CPlayerPed.h:
+
+jni/game/CPhysical.h:
+
+jni/game/CEntryInfo.h:
+
+jni/game/CEntity.h:
+
+jni/game/CPlaceable.h:
+
+jni/game/math/matrix.h:
+
+jni/plugin/pools/../remoteplayer.h:
+
+jni/vendor/EASTL/unordered_map.h:
+
+jni/vendor/EASTL/hash_map.h:
+
+jni/vendor/EASTL/internal/hashtable.h:
+
+jni/vendor/EASTL/tuple.h:
+
+jni/plugin.h:
+
+jni/offsets.h:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/vector:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__bit_reference:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__split_buffer:
+
+jni/xorstr.h:

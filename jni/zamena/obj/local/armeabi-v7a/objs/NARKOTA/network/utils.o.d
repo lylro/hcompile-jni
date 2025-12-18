ed76de5a33e77b9f4ccf565beebebc942c146da0
arseniy-cage <lllufoxnewyear2023@gmail.com>
2025-12-18 20:23:32 +1000

    Clean repository 2025-12-18 20:23:32

--- jni/zamena/obj/local/armeabi-v7a/objs/NARKOTA/network/utils.o.d
+++
@@ -1,5 +0,0 @@
-./obj/local/armeabi-v7a/objs/NARKOTA/network/utils.o: \
-  jni/network/utils.cpp \
-  C:/ndk16/build//../sources/cxx-stl/llvm-libc++/include\stdio.h
-
-C:/ndk16/build//../sources/cxx-stl/llvm-libc++/include\stdio.h:

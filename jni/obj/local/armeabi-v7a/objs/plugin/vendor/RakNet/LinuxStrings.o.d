94065c523820e00758a2d7f939c3115082579c27
arseniy-cage <lllufoxnewyear2023@gmail.com>
2025-12-18 22:25:32 +1000

    Clean repository 2025-12-18 22:25:32

--- jni/obj/local/armeabi-v7a/objs/plugin/vendor/RakNet/LinuxStrings.o.d
+++
@@ -1,8 +0,0 @@
-./obj/local/armeabi-v7a/objs/plugin/vendor/RakNet/LinuxStrings.o: \
-  jni/vendor/RakNet/LinuxStrings.cpp \
-  C:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/string.h \
-  C:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/__config
-
-C:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/string.h:
-
-C:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/__config:

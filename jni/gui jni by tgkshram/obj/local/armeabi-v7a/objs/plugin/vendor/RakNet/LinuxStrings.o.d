cd1b690086821c720f8e515a8aca966ad62de6f7
arseniy-cage <lllufoxnewyear2023@gmail.com>
2025-12-18 18:58:50 +1000

    Clean repository 2025-12-18 18:58:50

--- jni/gui jni by tgkshram/obj/local/armeabi-v7a/objs/plugin/vendor/RakNet/LinuxStrings.o.d
+++
@@ -1,8 +0,0 @@
-./obj/local/armeabi-v7a/objs/plugin/vendor/RakNet/LinuxStrings.o: \
-  jni/vendor/RakNet/LinuxStrings.cpp \
-  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/string.h \
-  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__config
-
-C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/string.h:
-
-C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__config:

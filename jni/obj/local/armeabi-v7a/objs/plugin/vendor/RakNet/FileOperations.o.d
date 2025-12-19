7363ee0ac5e82b740568bc49b6a516ddf27d7dd1
arseniy-cage <lllufoxnewyear2023@gmail.com>
2025-12-19 17:44:04 +1000

    JNI build 2025-12-19 17:44:04

---
+++ jni/obj/local/armeabi-v7a/objs/plugin/vendor/RakNet/FileOperations.o.d
@@ -0,0 +1,21 @@
+./obj/local/armeabi-v7a/objs/plugin/vendor/RakNet/FileOperations.o: \
+  jni/vendor/RakNet/FileOperations.cpp jni/vendor/RakNet/_findfirst.h \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/string.h \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__config \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stdio.h \
+  jni/vendor/RakNet/FileOperations.h jni/vendor/RakNet/Export.h \
+  jni/vendor/RakNet/LinuxStrings.h
+
+jni/vendor/RakNet/_findfirst.h:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/string.h:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__config:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stdio.h:
+
+jni/vendor/RakNet/FileOperations.h:
+
+jni/vendor/RakNet/Export.h:
+
+jni/vendor/RakNet/LinuxStrings.h:

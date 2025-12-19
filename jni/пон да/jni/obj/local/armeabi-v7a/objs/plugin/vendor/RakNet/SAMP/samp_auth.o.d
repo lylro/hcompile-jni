8e47b82413fc4f31626b5d1b76aa90824cbf1175
arseniy-cage <lllufoxnewyear2023@gmail.com>
2025-12-20 07:16:37 +1000

    JNI build 2025-12-20 07:16:37

---
+++ jni/пон да/jni/obj/local/armeabi-v7a/objs/plugin/vendor/RakNet/SAMP/samp_auth.o.d
@@ -0,0 +1,20 @@
+./obj/local/armeabi-v7a/objs/plugin/vendor/RakNet/SAMP/samp_auth.o: \
+  jni/vendor/RakNet/SAMP/samp_auth.cpp \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cstdint \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__config \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stdint.h \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cstring \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/string.h \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stdio.h
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cstdint:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__config:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stdint.h:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cstring:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/string.h:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stdio.h:

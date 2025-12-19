543c5a6f718b448756a8f3fdd274baa7f1e7a8c6
arseniy-cage <lllufoxnewyear2023@gmail.com>
2025-12-20 04:57:37 +1000

    JNI build 2025-12-20 04:57:37

---
+++ jni/jni/obj/local/armeabi-v7a/objs/plugin/vendor/RakNet/DS_ByteQueue.o.d
@@ -0,0 +1,19 @@
+./obj/local/armeabi-v7a/objs/plugin/vendor/RakNet/DS_ByteQueue.o: \
+  jni/vendor/RakNet/DS_ByteQueue.cpp jni/vendor/RakNet/DS_ByteQueue.h \
+  jni/vendor/RakNet/Export.h \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/string.h \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__config \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stdlib.h \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stdio.h
+
+jni/vendor/RakNet/DS_ByteQueue.h:
+
+jni/vendor/RakNet/Export.h:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/string.h:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__config:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stdlib.h:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stdio.h:
---
+++ jni/jni/obj/local/armeabi-v7a/objs/plugin/vendor/imgui/imgui_tables.o.d
@@ -0,0 +1,37 @@
+./obj/local/armeabi-v7a/objs/plugin/vendor/imgui/imgui_tables.o: \
+  jni/vendor/imgui/imgui_tables.cpp jni/vendor/imgui/imgui.h \
+  jni/vendor/imgui/imconfig.h \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/float.h \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__config \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stddef.h \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/string.h \
+  jni/vendor/imgui/imgui_internal.h \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stdio.h \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stdlib.h \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/math.h \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/limits.h \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stdint.h
+
+jni/vendor/imgui/imgui.h:
+
+jni/vendor/imgui/imconfig.h:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/float.h:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__config:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stddef.h:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/string.h:
+
+jni/vendor/imgui/imgui_internal.h:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stdio.h:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stdlib.h:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/math.h:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/limits.h:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stdint.h:

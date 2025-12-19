99a59accc882778ded9cd587c7203ba733e4b4db
arseniy-cage <lllufoxnewyear2023@gmail.com>
2025-12-19 18:45:52 +1000

    JNI build 2025-12-19 18:45:52

---
+++ jni/jni/obj/local/armeabi-v7a/objs/plugin/vendor/Substrate/SubstratePosixMemory.o.d
@@ -0,0 +1,20 @@
+./obj/local/armeabi-v7a/objs/plugin/vendor/Substrate/SubstratePosixMemory.o: \
+  jni/vendor/Substrate/SubstratePosixMemory.cpp \
+  jni/vendor/Substrate/CydiaSubstrate.h \
+  C:/ndk25/build/../sources/cxx-stl/llvm-libc++/include/stdlib.h \
+  C:/ndk25/build/../sources/cxx-stl/llvm-libc++/include/__config \
+  jni/vendor/Substrate/SubstrateLog.hpp \
+  C:/ndk25/build/../sources/cxx-stl/llvm-libc++/include/errno.h \
+  C:/ndk25/build/../sources/cxx-stl/llvm-libc++/include/stdio.h
+
+jni/vendor/Substrate/CydiaSubstrate.h:
+
+C:/ndk25/build/../sources/cxx-stl/llvm-libc++/include/stdlib.h:
+
+C:/ndk25/build/../sources/cxx-stl/llvm-libc++/include/__config:
+
+jni/vendor/Substrate/SubstrateLog.hpp:
+
+C:/ndk25/build/../sources/cxx-stl/llvm-libc++/include/errno.h:
+
+C:/ndk25/build/../sources/cxx-stl/llvm-libc++/include/stdio.h:
---
+++ jni/jni/obj/local/armeabi-v7a/objs/plugin/vendor/imgui/imgui.o.d
@@ -0,0 +1,37 @@
+./obj/local/armeabi-v7a/objs/plugin/vendor/imgui/imgui.o: \
+  jni/vendor/imgui/imgui.cpp jni/vendor/imgui/imgui.h \
+  jni/vendor/imgui/imconfig.h \
+  C:/ndk25/build/../sources/cxx-stl/llvm-libc++/include/float.h \
+  C:/ndk25/build/../sources/cxx-stl/llvm-libc++/include/__config \
+  C:/ndk25/build/../sources/cxx-stl/llvm-libc++/include/stddef.h \
+  C:/ndk25/build/../sources/cxx-stl/llvm-libc++/include/string.h \
+  jni/vendor/imgui/imgui_internal.h \
+  C:/ndk25/build/../sources/cxx-stl/llvm-libc++/include/stdio.h \
+  C:/ndk25/build/../sources/cxx-stl/llvm-libc++/include/stdlib.h \
+  C:/ndk25/build/../sources/cxx-stl/llvm-libc++/include/math.h \
+  C:/ndk25/build/../sources/cxx-stl/llvm-libc++/include/limits.h \
+  C:/ndk25/build/../sources/cxx-stl/llvm-libc++/include/stdint.h
+
+jni/vendor/imgui/imgui.h:
+
+jni/vendor/imgui/imconfig.h:
+
+C:/ndk25/build/../sources/cxx-stl/llvm-libc++/include/float.h:
+
+C:/ndk25/build/../sources/cxx-stl/llvm-libc++/include/__config:
+
+C:/ndk25/build/../sources/cxx-stl/llvm-libc++/include/stddef.h:
+
+C:/ndk25/build/../sources/cxx-stl/llvm-libc++/include/string.h:
+
+jni/vendor/imgui/imgui_internal.h:
+
+C:/ndk25/build/../sources/cxx-stl/llvm-libc++/include/stdio.h:
+
+C:/ndk25/build/../sources/cxx-stl/llvm-libc++/include/stdlib.h:
+
+C:/ndk25/build/../sources/cxx-stl/llvm-libc++/include/math.h:
+
+C:/ndk25/build/../sources/cxx-stl/llvm-libc++/include/limits.h:
+
+C:/ndk25/build/../sources/cxx-stl/llvm-libc++/include/stdint.h:

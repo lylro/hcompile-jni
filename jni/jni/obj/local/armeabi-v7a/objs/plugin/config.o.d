5d61b021c4a877cb1acdead4dd34bd658519fa11
arseniy-cage <lllufoxnewyear2023@gmail.com>
2025-12-20 04:49:59 +1000

    JNI build 2025-12-20 04:49:59

---
+++ jni/jni/obj/local/armeabi-v7a/objs/plugin/config.o.d
@@ -0,0 +1,22 @@
+./obj/local/armeabi-v7a/objs/plugin/config.o: jni/config.cpp \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/limits.h \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__config \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stdio.h \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stdlib.h \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/string.h \
+  jni/config.h \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stdbool.h
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/limits.h:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__config:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stdio.h:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stdlib.h:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/string.h:
+
+jni/config.h:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stdbool.h:
---
+++ jni/jni/obj/local/armeabi-v7a/objs/plugin/vendor/RakNet/SHA1.o.d
@@ -0,0 +1,21 @@
+./obj/local/armeabi-v7a/objs/plugin/vendor/RakNet/SHA1.o: \
+  jni/vendor/RakNet/SHA1.cpp jni/vendor/RakNet/SHA1.h \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stdio.h \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__config \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/string.h \
+  jni/vendor/RakNet/Types.h jni/vendor/RakNet/Export.h \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stdlib.h
+
+jni/vendor/RakNet/SHA1.h:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stdio.h:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__config:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/string.h:
+
+jni/vendor/RakNet/Types.h:
+
+jni/vendor/RakNet/Export.h:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stdlib.h:

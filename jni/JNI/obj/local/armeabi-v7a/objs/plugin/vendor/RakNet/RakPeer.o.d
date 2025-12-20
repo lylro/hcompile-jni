4e607b3d445e6ad9a3d9fc8c93c2a1313fa02307
arseniy-cage <lllufoxnewyear2023@gmail.com>
2025-12-20 21:12:52 +1000

    JNI build 2025-12-20 21:12:52

---
+++ jni/JNI/obj/local/armeabi-v7a/objs/plugin/vendor/RakNet/RakPeer.o.d
@@ -0,0 +1,278 @@
+./obj/local/armeabi-v7a/objs/plugin/vendor/RakNet/RakPeer.o: \
+  jni/vendor/RakNet/RakPeer.cpp jni/vendor/RakNet/RakNetDefines.h \
+  jni/vendor/RakNet/RakPeer.h jni/vendor/RakNet/Export.h \
+  jni/vendor/RakNet/RakPeerInterface.h \
+  jni/vendor/RakNet/PacketPriority.h jni/vendor/RakNet/NetworkTypes.h \
+  jni/vendor/RakNet/ReliabilityLayer.h jni/vendor/RakNet/SocketLayer.h \
+  jni/vendor/RakNet/ClientContextStruct.h jni/vendor/RakNet/MTUSize.h \
+  jni/vendor/RakNet/DS_LinkedList.h jni/vendor/RakNet/DS_List.h \
+  D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/string.h \
+  D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/__config \
+  jni/vendor/RakNet/DS_Queue.h jni/vendor/RakNet/BitStream.h \
+  D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/math.h \
+  D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/float.h \
+  jni/vendor/RakNet/InternalPacket.h \
+  jni/vendor/RakNet/InternalPacketPool.h \
+  jni/vendor/RakNet/DataBlockEncryptor.h jni/vendor/RakNet/rijndael.h \
+  D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/stdio.h \
+  jni/vendor/RakNet/RakNetStatistics.h jni/vendor/RakNet/SHA1.h \
+  jni/vendor/RakNet/Types.h jni/vendor/RakNet/DS_OrderedList.h \
+  jni/vendor/RakNet/DS_RangeList.h jni/vendor/RakNet/DS_BPlusTree.h \
+  jni/vendor/RakNet/DS_MemoryPool.h jni/vendor/RakNet/RPCNode.h \
+  jni/vendor/RakNet/RSACrypt.h jni/vendor/RakNet/BigTypes.h \
+  jni/vendor/RakNet/Rand.h jni/vendor/RakNet/SingleProducerConsumer.h \
+  jni/vendor/RakNet/RPCMap.h jni/vendor/RakNet/SimpleMutex.h \
+  jni/plugin/common.h \
+  D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/cstdint \
+  D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/stdint.h \
+  D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/string \
+  D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/string_view \
+  D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/__string \
+  D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/algorithm \
+  D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/initializer_list \
+  D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/cstddef \
+  D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/type_traits \
+  D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/cstring \
+  D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/utility \
+  D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/__tuple \
+  D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/version \
+  D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/__debug \
+  D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/iosfwd \
+  D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/wchar.h \
+  D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/memory \
+  D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/typeinfo \
+  D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/exception \
+  D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/cstdlib \
+  D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/stdlib.h \
+  D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/new \
+  D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/limits \
+  D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/iterator \
+  D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/__functional_base \
+  D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/tuple \
+  D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/stdexcept \
+  D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/atomic \
+  D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/__threading_support \
+  D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/chrono \
+  D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/ctime \
+  D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/ratio \
+  D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/climits \
+  D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/limits.h \
+  D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/errno.h \
+  D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/functional \
+  D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/bit \
+  D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/cstdio \
+  D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/cwchar \
+  D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/cwctype \
+  D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/cctype \
+  D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/ctype.h \
+  D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/wctype.h \
+  jni/vendor/RakNet/SAMP/SAMPRPC.h jni/game/math/vector.h \
+  D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/cmath \
+  jni/xorstr.h jni/vendor/RakNet/GetTime.h \
+  jni/vendor/RakNet/PacketEnumerations.h \
+  jni/vendor/RakNet/DS_HuffmanEncodingTree.h \
+  jni/vendor/RakNet/DS_HuffmanEncodingTreeNode.h \
+  jni/vendor/RakNet/PluginInterface.h \
+  jni/vendor/RakNet/StringCompressor.h jni/vendor/RakNet/DS_Map.h \
+  jni/vendor/RakNet/StringTable.h jni/vendor/RakNet/NetworkIDGenerator.h \
+  jni/vendor/RakNet/DS_BinarySearchTree.h \
+  jni/vendor/RakNet/DS_QueueLinkedList.h jni/vendor/RakNet/RakSleep.h \
+  jni/vendor/RakNet/RouterInterface.h jni/vendor/RakNet/RakAssert.h
+
+jni/vendor/RakNet/RakNetDefines.h:
+
+jni/vendor/RakNet/RakPeer.h:
+
+jni/vendor/RakNet/Export.h:
+
+jni/vendor/RakNet/RakPeerInterface.h:
+
+jni/vendor/RakNet/PacketPriority.h:
+
+jni/vendor/RakNet/NetworkTypes.h:
+
+jni/vendor/RakNet/ReliabilityLayer.h:
+
+jni/vendor/RakNet/SocketLayer.h:
+
+jni/vendor/RakNet/ClientContextStruct.h:
+
+jni/vendor/RakNet/MTUSize.h:
+
+jni/vendor/RakNet/DS_LinkedList.h:
+
+jni/vendor/RakNet/DS_List.h:
+
+D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/string.h:
+
+D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/__config:
+
+jni/vendor/RakNet/DS_Queue.h:
+
+jni/vendor/RakNet/BitStream.h:
+
+D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/math.h:
+
+D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/float.h:
+
+jni/vendor/RakNet/InternalPacket.h:
+
+jni/vendor/RakNet/InternalPacketPool.h:
+
+jni/vendor/RakNet/DataBlockEncryptor.h:
+
+jni/vendor/RakNet/rijndael.h:
+
+D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/stdio.h:
+
+jni/vendor/RakNet/RakNetStatistics.h:
+
+jni/vendor/RakNet/SHA1.h:
+
+jni/vendor/RakNet/Types.h:
+
+jni/vendor/RakNet/DS_OrderedList.h:
+
+jni/vendor/RakNet/DS_RangeList.h:
+
+jni/vendor/RakNet/DS_BPlusTree.h:
+
+jni/vendor/RakNet/DS_MemoryPool.h:
+
+jni/vendor/RakNet/RPCNode.h:
+
+jni/vendor/RakNet/RSACrypt.h:
+
+jni/vendor/RakNet/BigTypes.h:
+
+jni/vendor/RakNet/Rand.h:
+
+jni/vendor/RakNet/SingleProducerConsumer.h:
+
+jni/vendor/RakNet/RPCMap.h:
+
+jni/vendor/RakNet/SimpleMutex.h:
+
+jni/plugin/common.h:
+
+D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/cstdint:
+
+D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/stdint.h:
+
+D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/string:
+
+D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/string_view:
+
+D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/__string:
+
+D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/algorithm:
+
+D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/initializer_list:
+
+D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/cstddef:
+
+D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/type_traits:
+
+D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/cstring:
+
+D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/utility:
+
+D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/__tuple:
+
+D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/version:
+
+D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/__debug:
+
+D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/iosfwd:
+
+D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/wchar.h:
+
+D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/memory:
+
+D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/typeinfo:
+
+D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/exception:
+
+D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/cstdlib:
+
+D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/stdlib.h:
+
+D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/new:
+
+D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/limits:
+
+D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/iterator:
+
+D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/__functional_base:
+
+D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/tuple:
+
+D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/stdexcept:
+
+D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/atomic:
+
+D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/__threading_support:
+
+D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/chrono:
+
+D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/ctime:
+
+D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/ratio:
+
+D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/climits:
+
+D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/limits.h:
+
+D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/errno.h:
+
+D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/functional:
+
+D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/bit:
+
+D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/cstdio:
+
+D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/cwchar:
+
+D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/cwctype:
+
+D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/cctype:
+
+D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/ctype.h:
+
+D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/wctype.h:
+
+jni/vendor/RakNet/SAMP/SAMPRPC.h:
+
+jni/game/math/vector.h:
+
+D:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/cmath:
+
+jni/xorstr.h:
+
+jni/vendor/RakNet/GetTime.h:
+
+jni/vendor/RakNet/PacketEnumerations.h:
+
+jni/vendor/RakNet/DS_HuffmanEncodingTree.h:
+
+jni/vendor/RakNet/DS_HuffmanEncodingTreeNode.h:
+
+jni/vendor/RakNet/PluginInterface.h:
+
+jni/vendor/RakNet/StringCompressor.h:
+
+jni/vendor/RakNet/DS_Map.h:
+
+jni/vendor/RakNet/StringTable.h:
+
+jni/vendor/RakNet/NetworkIDGenerator.h:
+
+jni/vendor/RakNet/DS_BinarySearchTree.h:
+
+jni/vendor/RakNet/DS_QueueLinkedList.h:
+
+jni/vendor/RakNet/RakSleep.h:
+
+jni/vendor/RakNet/RouterInterface.h:
+
+jni/vendor/RakNet/RakAssert.h:

563c33f5ac5f713fbde4686c106555b9eba3601c
arseniy-cage <lllufoxnewyear2023@gmail.com>
2025-12-21 21:50:32 +1000

    JNI build 2025-12-21 21:50:32

---
+++ jni/jni privat/obj/local/armeabi-v7a/objs/plugin/game/math/matrix.o.d
@@ -0,0 +1,21 @@
+./obj/local/armeabi-v7a/objs/plugin/game/math/matrix.o: \
+  jni/game/math/matrix.cpp jni/game/math/matrix.h jni/game/math/vector.h \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cmath \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__config \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/math.h \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/version \
+  C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/type_traits
+
+jni/game/math/matrix.h:
+
+jni/game/math/vector.h:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cmath:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__config:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/math.h:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/version:
+
+C:/android-ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/type_traits:

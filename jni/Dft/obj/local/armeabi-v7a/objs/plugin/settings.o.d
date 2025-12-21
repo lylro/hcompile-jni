4e6fe2005e44cf12a853fab1f4300fc52ea24df0
arseniy-cage <lllufoxnewyear2023@gmail.com>
2025-12-21 19:28:55 +1000

    JNI build 2025-12-21 19:28:55

---
+++ jni/Dft/obj/local/armeabi-v7a/objs/plugin/settings.o.d
@@ -0,0 +1,31 @@
+./obj/local/armeabi-v7a/objs/plugin/settings.o: jni/settings.cpp \
+  jni/config.h \
+  C:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/stdbool.h \
+  C:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/__config \
+  jni/settings.h \
+  C:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/cstring \
+  C:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/string.h \
+  C:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/cstdio \
+  C:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/stdio.h \
+  C:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/cstdlib \
+  C:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/stdlib.h
+
+jni/config.h:
+
+C:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/stdbool.h:
+
+C:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/__config:
+
+jni/settings.h:
+
+C:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/cstring:
+
+C:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/string.h:
+
+C:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/cstdio:
+
+C:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/stdio.h:
+
+C:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/cstdlib:
+
+C:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/stdlib.h:
---
+++ jni/Dft/obj/local/armeabi-v7a/objs/plugin/vendor/RakNet/rijndael.o.d
@@ -0,0 +1,24 @@
+./obj/local/armeabi-v7a/objs/plugin/vendor/RakNet/rijndael.o: \
+  jni/vendor/RakNet/rijndael.cpp \
+  C:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/stdio.h \
+  C:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/__config \
+  C:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/stdlib.h \
+  C:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/string.h \
+  jni/vendor/RakNet/rijndael.h jni/vendor/RakNet/Rand.h \
+  jni/vendor/RakNet/Export.h jni/vendor/RakNet/rijndael-boxes.h
+
+C:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/stdio.h:
+
+C:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/__config:
+
+C:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/stdlib.h:
+
+C:/ndk25c/build/../sources/cxx-stl/llvm-libc++/include/string.h:
+
+jni/vendor/RakNet/rijndael.h:
+
+jni/vendor/RakNet/Rand.h:
+
+jni/vendor/RakNet/Export.h:
+
+jni/vendor/RakNet/rijndael-boxes.h:

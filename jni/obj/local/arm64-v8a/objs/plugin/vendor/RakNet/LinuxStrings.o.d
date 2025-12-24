7b91cc79254ad38dfe3171fb70f547675418d9fb
arseniy-cage <lllufoxnewyear2023@gmail.com>
2025-12-25 07:10:29 +1000

    Clean repository 2025-12-25 07:10:29

--- jni/obj/local/arm64-v8a/objs/plugin/vendor/RakNet/LinuxStrings.o.d
+++
@@ -1,8 +0,0 @@
-./obj/local/arm64-v8a/objs/plugin/vendor/RakNet/LinuxStrings.o: \
-  jni/vendor/RakNet/LinuxStrings.cpp \
-  C:/n25/build/../sources/cxx-stl/llvm-libc++/include/string.h \
-  C:/n25/build/../sources/cxx-stl/llvm-libc++/include/__config
-
-C:/n25/build/../sources/cxx-stl/llvm-libc++/include/string.h:
-
-C:/n25/build/../sources/cxx-stl/llvm-libc++/include/__config:
--- jni/obj/local/arm64-v8a/objs/plugin/vendor/RakNet/SimpleMutex.o.d
+++
@@ -1,7 +0,0 @@
-./obj/local/arm64-v8a/objs/plugin/vendor/RakNet/SimpleMutex.o: \
-  jni/vendor/RakNet/SimpleMutex.cpp jni/vendor/RakNet/SimpleMutex.h \
-  jni/vendor/RakNet/Export.h
-
-jni/vendor/RakNet/SimpleMutex.h:
-
-jni/vendor/RakNet/Export.h:

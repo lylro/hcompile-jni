ae2cb1fb4d0bda3d05e37d9cfe6c6ef200562858
arseniy-cage <lllufoxnewyear2023@gmail.com>
2025-12-25 03:09:08 +1000

    Clean repository 2025-12-25 03:09:08

--- jni/64_jni/obj/local/arm64-v8a/objs/plugin/vendor/RakNet/AsynchronousFileIO.o.d
+++
@@ -1,2 +0,0 @@
-./obj/local/arm64-v8a/objs/plugin/vendor/RakNet/AsynchronousFileIO.o: \
-  jni/vendor/RakNet/AsynchronousFileIO.cpp
--- jni/64_jni/obj/local/arm64-v8a/objs/plugin/vendor/RakNet/ExtendedOverlappedPool.o.d
+++
@@ -1,2 +0,0 @@
-./obj/local/arm64-v8a/objs/plugin/vendor/RakNet/ExtendedOverlappedPool.o: \
-  jni/vendor/RakNet/ExtendedOverlappedPool.cpp

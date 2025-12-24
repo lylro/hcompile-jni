e9247a71d902d330f72be889eb4e1fa1c7f5d395
arseniy-cage <lllufoxnewyear2023@gmail.com>
2025-12-25 01:02:16 +1000

    JNI build 2025-12-25 01:02:16

---
+++ jni/PRIVATE SLIV/jni/obj/local/armeabi-v7a/objs/plugin/game/CPlayerPed.o.d
@@ -0,0 +1,179 @@
+./obj/local/armeabi-v7a/objs/plugin/game/CPlayerPed.o: \
+  jni/game/CPlayerPed.cpp jni/game/CPlayerPed.h jni/game/CPhysical.h \
+  jni/game/CEntryInfo.h jni/game/CEntity.h \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cstdint \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__config \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stdint.h \
+  jni/game/CPlaceable.h jni/game/math/matrix.h jni/game/math/vector.h \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cmath \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/math.h \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/version \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/type_traits \
+  jni/xorstr.h jni/plugin.h \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/string.h \
+  jni/offsets.h \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/vector \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/iosfwd \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/wchar.h \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__bit_reference \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/bit \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/limits \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__debug \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/algorithm \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/initializer_list \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cstddef \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cstring \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/utility \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__tuple \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/memory \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/typeinfo \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/exception \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cstdlib \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stdlib.h \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/new \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/iterator \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__functional_base \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/tuple \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stdexcept \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/atomic \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__threading_support \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/chrono \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/ctime \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/ratio \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/climits \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/limits.h \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/errno.h \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/functional \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__split_buffer \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/string \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/string_view \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__string \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cstdio \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stdio.h \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cwchar \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cwctype \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cctype \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/ctype.h \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/wctype.h
+
+jni/game/CPlayerPed.h:
+
+jni/game/CPhysical.h:
+
+jni/game/CEntryInfo.h:
+
+jni/game/CEntity.h:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cstdint:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__config:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stdint.h:
+
+jni/game/CPlaceable.h:
+
+jni/game/math/matrix.h:
+
+jni/game/math/vector.h:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cmath:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/math.h:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/version:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/type_traits:
+
+jni/xorstr.h:
+
+jni/plugin.h:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/string.h:
+
+jni/offsets.h:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/vector:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/iosfwd:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/wchar.h:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__bit_reference:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/bit:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/limits:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__debug:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/algorithm:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/initializer_list:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cstddef:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cstring:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/utility:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__tuple:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/memory:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/typeinfo:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/exception:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cstdlib:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stdlib.h:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/new:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/iterator:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__functional_base:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/tuple:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stdexcept:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/atomic:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__threading_support:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/chrono:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/ctime:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/ratio:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/climits:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/limits.h:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/errno.h:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/functional:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__split_buffer:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/string:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/string_view:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__string:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cstdio:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stdio.h:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cwchar:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cwctype:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/cctype:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/ctype.h:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/wctype.h:
---
+++ jni/PRIVATE SLIV/jni/obj/local/armeabi-v7a/objs/plugin/vendor/And64InlineHook/And64InlineHook.o.d
@@ -0,0 +1,17 @@
+./obj/local/armeabi-v7a/objs/plugin/vendor/And64InlineHook/And64InlineHook.o: \
+  jni/vendor/And64InlineHook/And64InlineHook.cpp \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/inttypes.h \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__config \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stdlib.h \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/string.h \
+  C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/errno.h
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/inttypes.h:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/__config:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/stdlib.h:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/string.h:
+
+C:/ndk-r25c/build/../sources/cxx-stl/llvm-libc++/include/errno.h:

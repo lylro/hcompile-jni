f8f7aa5abca402419cb5180a55ffcf6926f9cd37
arseniy-cage <lllufoxnewyear2023@gmail.com>
2025-12-25 07:18:35 +1000

    Clean repository 2025-12-25 07:18:35

--- jni/obj/local/arm64-v8a/objs/plugin/vendor/RakNet/CheckSum.o.d
+++
@@ -1,4 +0,0 @@
-./obj/local/arm64-v8a/objs/plugin/vendor/RakNet/CheckSum.o: \
-  jni/vendor/RakNet/CheckSum.cpp jni/vendor/RakNet/CheckSum.h
-
-jni/vendor/RakNet/CheckSum.h:
--- jni/obj/local/arm64-v8a/objs/plugin/vendor/RakNet/SAMP/SAMPRPC.o.d
+++
@@ -1,2 +0,0 @@
-./obj/local/arm64-v8a/objs/plugin/vendor/RakNet/SAMP/SAMPRPC.o: \
-  jni/vendor/RakNet/SAMP/SAMPRPC.cpp

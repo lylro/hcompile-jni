da6b1a7d41d2d8cc61282877bc3219208a9dd09e
arseniy-cage <lllufoxnewyear2023@gmail.com>
2025-12-25 03:00:03 +1000

    Clean repository 2025-12-25 03:00:03

--- jni/64_jni/obj/local/arm64-v8a/objs/plugin/vendor/RakNet/EncodeClassName.o.d
+++
@@ -1,2 +0,0 @@
-./obj/local/arm64-v8a/objs/plugin/vendor/RakNet/EncodeClassName.o: \
-  jni/vendor/RakNet/EncodeClassName.cpp
--- jni/64_jni/obj/local/arm64-v8a/objs/plugin/vendor/RakNet/SystemDatabaseClient.o.d
+++
@@ -1,5 +0,0 @@
-./obj/local/arm64-v8a/objs/plugin/vendor/RakNet/SystemDatabaseClient.o: \
-  jni/vendor/RakNet/SystemDatabaseClient.cpp \
-  jni/vendor/RakNet/SystemDatabaseClient.h
-
-jni/vendor/RakNet/SystemDatabaseClient.h:

915fd0bbd6cb8df921dfe705617141895a89c942
arseniy-cage <lllufoxnewyear2023@gmail.com>
2026-01-06 03:03:59 +1000

    JNI build 2026-01-06 03:03:59

---
+++ jni/jni/CSettingsLoader.h
@@ -0,0 +1,18 @@
+//
+// Created by Regerds on 14.12.2024.
+//
+
+#ifndef BLACK_RUSSIA_CSETTINGSLOADER_H
+#define BLACK_RUSSIA_CSETTINGSLOADER_H
+
+
+class CSettingsLoader {
+public:
+    CSettingsLoader();
+    ~CSettingsLoader();
+
+    int water, sky, aa, lowerdd, lowercars;
+};
+
+
+#endif //BLACK_RUSSIA_CSETTINGSLOADER_H

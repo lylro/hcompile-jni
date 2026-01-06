4173242b58f8c92faeed7a311df6a5e65f98f013
arseniy-cage <lllufoxnewyear2023@gmail.com>
2026-01-06 18:26:55 +1000

    JNI build 2026-01-06 18:26:55

---
+++ jni/jni/gui/gui.h
@@ -0,0 +1,96 @@
+#pragma once
+#include "../vendor/imgui/imgui.h"
+#include "..//vendor/raknet/SingleProducerConsumer.h"
+#include "UIUtils.h"
+
+enum eTouchType
+{
+	TOUCH_POP = 1,
+	TOUCH_PUSH = 2,
+	TOUCH_MOVE = 3
+};
+
+struct BUFFERED_COMMAND_TEXTDRAW
+{
+	uint16_t textdrawId;
+};
+
+class CGUI
+{
+public:
+	CGUI();
+	~CGUI();
+
+	void Render();
+		void ShowMenucar();
+	
+	//speed
+	void ShowSpeed();
+	void SetHealth(float fHealth);
+	int GetHealth();
+	int bHealth;
+	void SetEngine(int engine);
+	int bEngine;
+	void SetLights(int lights);
+	int bLights;
+	void SetDoor(int door);
+	int bDoor;
+	void SetMeliage(float meliage);
+    int bMeliage = 0;
+    void SetEat(float eat);
+    int GetEat();
+    int eat;
+    void SetFuel(float fuel);
+	int m_fuel;
+
+	float ScaleX(float x) { return m_vecScale.x * x; }
+	float ScaleY(float y) { return m_vecScale.y * y; }
+	ImFont* GetFont() { return m_pFont; }
+	ImFont* GetSampFont() { return m_pSampFont; }
+	float GetFontSize() { return m_fFontSize; }
+	float GetFontSizeNoScale() { return m_fFontSizeNoScale; }
+    ImFont* LoadFont(char *font, float fontsize);
+	ImFont* GetWeaponFont() { return m_pFontGTAWeap; }
+	bool OnTouchEvent(int type, bool multi, int x, int y);
+
+	void RenderText(ImVec2& pos, ImU32 col, bool bOutline, const char* text_begin, const char* text_end = nullptr);
+	void RenderTextForChatWindow(ImVec2& pos, ImU32 col, bool bOutline, const char* text_begin, const char* text_end = nullptr);
+	void RenderTextWithSize(ImVec2& posCur, ImU32 col, bool bOutline, const char* text_begin, const char* text_end, float font_size);
+
+	void RenderTextDeathMessage(ImVec2& pos, ImU32 col, bool bOutline, const char* text_begin, const char* text_end = nullptr, float font_size = 0.0f, ImFont *font = NULL, bool bOutlineUseTextColor = false);
+
+	void PushToBufferedQueueTextDrawPressed(uint16_t textdrawId);
+
+	void RenderTextForMaterialText(ImVec2& pos, ImU32 col, bool bOutline, const char* text_begin, const char* text_end = nullptr, float font_size = 0.0f, ImFont *font = NULL, bool bOutlineUseTextColor = false);
+
+	ImGuiWindow* m_imWindow = nullptr;
+private:
+	void PreProcessInput();
+	void PostProcessInput();
+	void RenderRakNetStatistics();
+	void RenderVersion();
+	void ProcessPushedTextdraws();
+	void ScrollDialog(float x, float y);
+	int m_iLastPosY;
+	
+
+private:
+	DataStructures::SingleProducerConsumer<BUFFERED_COMMAND_TEXTDRAW> m_BufferedCommandTextdraws;
+
+	struct RwTexture* m_pSplashTexture;
+	ImFont* 	m_pFont;
+	ImFont*		m_pSampFont;
+    ImFont*		m_pFontGTAWeap;
+	
+	ImVec2		m_vecScale;
+	float 		m_fFontSize;
+	float		m_fFontSizeNoScale;
+
+	bool		m_bNeedClearMousePos;
+	bool		m_bMouseDown;
+	ImVec2		m_vTouchPos;
+	bool		m_bNextClear;
+
+	bool 		m_bKeysStatus;
+	bool isShowbtn;
+};

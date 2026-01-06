8e7798bb3282dd845e06f9041179dd3e8038a06a
arseniy-cage <lllufoxnewyear2023@gmail.com>
2026-01-06 15:40:06 +1000

    JNI build 2026-01-06 15:40:06

---
+++ jni/jni/CGraphic.h
@@ -0,0 +1,9 @@
+class CGraphic {
+public:
+    void ApplyReflections(int reflection);
+    void ApplyResolution(int resolution);
+    void ApplyEffects(int effects);
+    void ChangeHud(int hud);
+
+private:
+};

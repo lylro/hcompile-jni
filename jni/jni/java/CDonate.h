3e833a19930538090c2ef41dd277119185c37bc5
arseniy-cage <lllufoxnewyear2023@gmail.com>
2026-01-06 18:36:21 +1000

    JNI build 2026-01-06 18:36:21

---
+++ jni/jni/java/CDonate.h
@@ -0,0 +1,16 @@
+#ifndef CDONATE_H
+#define CDONATE_H
+
+static class CDonate {
+
+public:
+    static void ShowDonate(int money, int donate);
+
+    static void UpdateDonate(int money, int donate);
+
+
+    static bool bIsToggle;
+    static jobject thiz;
+};
+
+#endif //

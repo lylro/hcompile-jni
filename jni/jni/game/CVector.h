b3cdd99bf76e221c7e044af5a20b5ead9f3fe069
arseniy-cage <lllufoxnewyear2023@gmail.com>
2026-01-06 18:31:06 +1000

    JNI build 2026-01-06 18:31:06

---
+++ jni/jni/game/CVector.h
@@ -0,0 +1,26 @@
+// -- -- -- -- -- --
+// Created by Loony-dev © 2021
+// VK: https://vk.com/loonydev
+// -- -- -- -
+
+#pragma once
+
+#include "../main.h"
+#include "common.h"
+
+class CVector {
+public:
+	CVector();
+	CVector(float value);
+	CVector(float x, float y, float z);
+
+	VECTOR Set(float value);
+	VECTOR Set(float x, float y, float z);
+
+	VECTOR Get();
+
+	VECTOR Zero();
+
+private:
+	VECTOR vector;
+};

7bda6ac1ffce034c10d5c13d84a596bede99159b
root <root@vps1388.maze-host.ru>
2026-01-10 08:56:45 +0300

    JNI build 2026-01-10 08:56:45

---
+++ jni/jni/CGraphic.h
@@ -0,0 +1,10 @@
+class CGraphic {
+public:
+    void ApplyReflections(int reflection);
+    void ApplyRange(int range);
+    void ApplyResolution(int resolution);
+    void ApplyEffects(int effects);
+    void ApplyShadows(int shadows);
+
+private:
+};
---
+++ jni/jni/clientlogic/ChatMessenger.cpp
@@ -0,0 +1,65 @@
+#include "ChatMessenger.h"
+#include "../main.h"
+#include "../gui/gui.h"
+#include "../game/game.h"
+#include "../net/netgame.h"
+#include "../game/RW/RenderWare.h"
+#include "../chatwindow.h"
+#include "../playertags.h"
+#include "../dialog.h"
+#include "../keyboard.h"
+#include "../settings.h"
+#include "../util/armhook.h"
+// t . me / r k k d e v
+extern CGUI* pGUI;
+extern CChatWindow* pChatWindow;
+
+char CLocalisation::m_szMessages[E_MSG::MSG_COUNT][MAX_LOCALISATION_LENGTH] = {
+	"{bfbeba}Соединение с BLACK RUSSIA",
+	"{bfbeba}Соединено. Подготовка к игре...",
+	"{bfbeba}Сервер закрыл соединение, перезайдите",
+	"{ffffff}Клиент {ff0000}BLACK RUSSIA {ffffff}запущен",
+	"{bfbeba}unused",
+	"{bfbeba}Вы были заблокированы на сервере",
+	"{bfbeba}Потеряно соединение с сервером. Переподключение через 15 секунд...",
+	"{bfbeba}Отсутствует соединение. Рекомендации: перезагрузите роутер, смените сеть или воспользуйтесь VPN",
+	"{bfbeba}Сервер полон. Повторное соединение..."
+};
+
+void CLocalisation::Initialise(const char* szFile)
+{
+	Log("Initializing loader | by Weikton");
+	char buff[MAX_LOCALISATION_LENGTH];
+
+	sprintf(&buff[0], "%sSAMP/%s", g_pszStorage, szFile);
+
+	FILE* pFile = fopen(&buff[0], "r");
+	if (!pFile)
+	{
+		Log("Localisation | Cannot initialise");
+		return;
+	}
+
+	for (int i = 0; i < E_MSG::MSG_COUNT; i++)
+	{
+		memset(m_szMessages[i], 0, MAX_LOCALISATION_LENGTH);
+	}
+	uint32_t counter = 0;
+	while (fgets(&buff[0], MAX_LOCALISATION_LENGTH, pFile) != NULL)
+	{
+		if (counter == E_MSG::MSG_COUNT)
+		{
+			break;
+		}
+
+		memcpy((void*)& m_szMessages[counter][0], (const void*)(&buff[0]), MAX_LOCALISATION_LENGTH);
+		counter++;
+	}
+	fclose(pFile);
+	Log("Localisation initialized");
+}
+
+char* CLocalisation::GetMessage(E_MSG msg)
+{
+	return &m_szMessages[msg][0];
+}

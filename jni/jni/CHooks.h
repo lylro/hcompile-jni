4841a3df04bc6ee1b0728403a87e5560cdd30e5b
root <root@vps1388.maze-host.ru>
2026-01-17 21:43:37 +0300

    Clean repository 2026-01-17 21:43:37

--- jni/jni/CHooks.h
+++
@@ -1,9 +0,0 @@
-#ifndef CHOOKS_H
-#define CHOOKS_H
-
-class CHooks {
-public:
-    static void InitHooksKuzia();
-};
-
-#endif // CHOOKS_H
--- jni/jni/vendor/SimpleAndroidLog/log.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-class Log
-{
-public:
-	static void print(const char*, ...);
-	static void fprint(const char* format, ...);
-	static void printn(const char* tag, const char* format, ...);
-};

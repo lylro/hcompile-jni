f868481f3404a3e7d16ccb9ba788553b49e1c3f3
root <root@vps1388.maze-host.ru>
2026-01-21 12:14:13 +0300

    JNI build 2026-01-21 12:14:13

---
+++ jni/santrope-tea-gtasa/encryption/CTinyEncrypt.o.d
@@ -0,0 +1,11 @@
+./obj/local/armeabi-v7a/objs/sampvoice/__/santrope-tea-gtasa/encryption/CTinyEncrypt.o: \
+  jni/../santrope-tea-gtasa/encryption/CTinyEncrypt.cpp \
+  jni/../santrope-tea-gtasa/encryption/CTinyEncrypt.h \
+  jni/../santrope-tea-gtasa/encryption/common.h \
+  C:/ndk/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\string
+
+jni/../santrope-tea-gtasa/encryption/CTinyEncrypt.h:
+
+jni/../santrope-tea-gtasa/encryption/common.h:
+
+C:/ndk/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\string:

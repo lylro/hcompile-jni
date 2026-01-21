6426bc3e0a70b1fd32504990c87625a0825561b6
root <root@vps1388.maze-host.ru>
2026-01-21 12:48:31 +0300

    JNI build 2026-01-21 12:48:31

---
+++ jni/jni/game/GTASA/Models/ClumpModelInfo.h
@@ -0,0 +1,26 @@
+//
+// Created by plaka on 21.02.2023.
+//
+
+#ifndef LIVERUSSIA_CLUMPMODELINFO_H
+#define LIVERUSSIA_CLUMPMODELINFO_H
+
+#include "BaseModelInfo.h"
+//#include "../RW/rwcore.h"
+
+struct tCompSearchStructById {
+    int32_t m_nId;
+    //RwFrame* m_pFrame;
+
+    //inline tCompSearchStructById(int32_t id, RwFrame* frame) : m_nId(id), m_pFrame(frame) {}
+};
+
+struct CClumpModelInfo : public CBaseModelInfo {
+    union {
+        char *m_animFileName;
+        unsigned int m_dwAnimFileIndex;
+    };
+};
+// 3C
+
+#endif //LIVERUSSIA_CLUMPMODELINFO_H

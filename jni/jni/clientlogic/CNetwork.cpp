b906a986cdbeab1fa5fd54cb2b5565fd77347ec1
root <root@vps1388.maze-host.ru>
2026-01-22 19:49:23 +0300

    JNI build 2026-01-22 19:49:23

---
+++ jni/jni/clientlogic/CNetwork.cpp
@@ -0,0 +1,14 @@
+#include "CNetwork.h"
+#include <stdint.h>
+
+#include "util/CJavaWrapper.h"
+
+const char* g_szServerNames[MAX_SERVERS] = {
+	"HISANNO RP | TOMSK",
+	"HISANNO RP | ARKHANGELSK"
+};
+
+const CSetServer::CServerInstanceEncrypted g_sEncryptedAddresses[MAX_SERVERS] = {
+	CSetServer::create("188.127.241.74", 1, 16, 1889, false),
+	CSetServer::create("188.127.241.74", 1, 16, 1889, false),	
+};

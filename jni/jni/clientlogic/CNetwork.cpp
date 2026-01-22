afe0c9609c2b1ab8a2b28821649612205d7c76c7
root <root@vps1388.maze-host.ru>
2026-01-22 16:33:54 +0300

    JNI build 2026-01-22 16:33:54

---
+++ jni/jni/clientlogic/CNetwork.cpp
@@ -0,0 +1,15 @@
+//rick was here
+#include "CNetwork.h"
+#include <stdint.h>
+
+#include "util/CJavaWrapper.h"
+
+const char* g_szServerNames[MAX_SERVERS] = {
+	"BLACK MOSCOW | RED",
+	"BLACK RUSSIA | TEST"
+};
+
+const CSetServer::CServerInstanceEncrypted g_sEncryptedAddresses[MAX_SERVERS] = {
+	CSetServer::create("51.75.232.64", 1, 16, 1185, false),
+	CSetServer::create("51.75.232.64", 1, 16, 1185, false)	
+};

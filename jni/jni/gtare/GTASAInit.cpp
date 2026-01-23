922bb3fa438b658a82c18c0911c2924cd7a67ef2
root <root@vps1388.maze-host.ru>
2026-01-23 15:59:43 +0300

    Clean repository 2026-01-23 15:59:43

--- jni/jni/gtare/GTASAInit.cpp
+++
@@ -1,7 +0,0 @@
-#include "../main.h"
-#include "CHud.h"
-void GTASAInit()
-{
-   // CModelInfo::ms_modelInfoPtrs = (CBaseModelInfo**)(g_libGTASA + 0x87BF48);
-    CHUD::Sprites = (CSprite2d**)(g_libGTASA + 0x8F0704);
-}

4c6de23c7956995e893339bdb9c86a55b370ed54
root <root@vps1388.maze-host.ru>
2026-01-26 07:29:50 +0300

    JNI build 2026-01-26 07:29:50

---
+++ jni/santrope-tea-gtasa/encryption/aes.o.d
@@ -0,0 +1,14 @@
+./obj/local/armeabi-v7a/objs/NARKOTA/__/santrope-tea-gtasa/encryption/aes.o: \
+  jni/../santrope-tea-gtasa/encryption/aes.c \
+  D:/ndk/build/../sources/cxx-stl/llvm-libc++/include/string.h \
+  D:/ndk/build/../sources/cxx-stl/llvm-libc++/include/__config \
+  jni/../santrope-tea-gtasa/encryption/aes.h \
+  D:/ndk/build/../sources/cxx-stl/llvm-libc++/include/stdint.h
+
+D:/ndk/build/../sources/cxx-stl/llvm-libc++/include/string.h:
+
+D:/ndk/build/../sources/cxx-stl/llvm-libc++/include/__config:
+
+jni/../santrope-tea-gtasa/encryption/aes.h:
+
+D:/ndk/build/../sources/cxx-stl/llvm-libc++/include/stdint.h:

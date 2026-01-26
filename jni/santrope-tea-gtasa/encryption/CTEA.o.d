8ecb9b4e610840fb82ff2303b41b84c184f23bfc
root <root@vps1388.maze-host.ru>
2026-01-26 21:53:07 +0300

    JNI build 2026-01-26 21:53:07

---
+++ jni/santrope-tea-gtasa/encryption/CTEA.o.d
@@ -0,0 +1,20 @@
+./obj/local/armeabi-v7a/objs/NARKOTA/__/santrope-tea-gtasa/encryption/CTEA.o: \
+  jni/../santrope-tea-gtasa/encryption/CTEA.cpp \
+  jni/../santrope-tea-gtasa/encryption/CTEA.h \
+  jni/../santrope-tea-gtasa/encryption/common.h \
+  C:/android-ndk-r16b/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\string \
+  jni/../santrope-tea-gtasa/encryption/..//..//jni/results/TINY_ENCRYPT_result.h \
+  jni/../santrope-tea-gtasa/encryption/..//..//jni/results/../util/armhook.h \
+  jni/../santrope-tea-gtasa/encryption/..//..//jni/results/TEARAK_result.h
+
+jni/../santrope-tea-gtasa/encryption/CTEA.h:
+
+jni/../santrope-tea-gtasa/encryption/common.h:
+
+C:/android-ndk-r16b/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\string:
+
+jni/../santrope-tea-gtasa/encryption/..//..//jni/results/TINY_ENCRYPT_result.h:
+
+jni/../santrope-tea-gtasa/encryption/..//..//jni/results/../util/armhook.h:
+
+jni/../santrope-tea-gtasa/encryption/..//..//jni/results/TEARAK_result.h:

e5c9b4e5b65bee30b38f2c5a6b73fb9a96fe08b6
root <root@vps1388.maze-host.ru>
2026-01-27 20:23:15 +0300

    JNI build 2026-01-27 20:23:15

---
+++ jni/jni/customrender/RQVertexBuffer.h
@@ -0,0 +1,9 @@
+//
+// Created by weikton on 22.06.2025.
+//
+
+#pragma once
+
+struct RQVertexBuffer {
+    uint32_t vtable;
+};
---
+++ jni/jni/java_systems/CSpawnSelector.h
@@ -0,0 +1,12 @@
+#pragma once
+
+// RPC
+#define RPC_SPAWN_SELECT  0x36
+#define sendRPC_SPAWNSELECTOR	0x42
+
+class CSpawnSelector
+{
+public:
+	static void Show(int home, int fraction);
+};
+

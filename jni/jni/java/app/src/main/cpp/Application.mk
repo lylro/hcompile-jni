e4531d23e4167bcb5882a1c15b2c3244498069bc
root <root@vps1388.maze-host.ru>
2026-01-28 16:02:06 +0300

    Clean repository 2026-01-28 16:02:06

--- jni/jni/java/app/src/main/cpp/Application.mk
+++
@@ -1,9 +0,0 @@
-APP_PLATFORM := android-23
-
-APP_STL := c++_shared
-
-APP_OPTIM := release
-
-APP_CPPFLAGS := -std=c++17 -frtti -fexceptions -Wall
-
-NDK_TOOLCHAIN_VERSION := clang
--- jni/jni/java/app/src/main/cpp/native-lib.cpp
+++
@@ -1,7 +0,0 @@
-#include <jni.h>
-#include <iostream>
-
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_android_quikky_MainActivity_stringFromJNI(JNIEnv* env, jobject) {
-    return env->NewStringUTF("");
-}

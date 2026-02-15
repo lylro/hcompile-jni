e11ca753e9ef176e977032018a1ee66d030193d5
root <root@vps1388.maze-host.ru>
2026-02-15 16:02:25 +0300

    Clean repository 2026-02-15 16:02:25

--- jni/jni/game/ES2VertexBuffer.cpp
+++
@@ -1,6 +0,0 @@
-#include "ES2VertexBuffer.h"
-#include "../vendor/armhook/patch.h"
-
-void ES2VertexBuffer::InjectHooks() {
-    CHook::Write(g_libGTASA + (VER_x32 ? 0x006777E0 : 0x84CFF8), &ES2VertexBuffer::curCPUBuffer);
-}

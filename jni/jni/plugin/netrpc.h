590d0a241e638fa6b19bf44755290cfe8a6822ee
root <root@vps1388.maze-host.ru>
2026-02-16 21:14:46 +0300

    Clean repository 2026-02-16 21:14:46

--- jni/jni/plugin/netrpc.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "vendor/RakNet/RakNetworkFactory.h"
-#include "vendor/RakNet/RakClient.h"
-#include "vendor/RakNet/SAMP/SAMPRPC.h"
-
-void RegisterRPCs(RakClientInterface* pInterface);
-

3afdcc63dccfaa47ab35404a58d9164f2d9cffff
root <root@vps1388.maze-host.ru>
2026-02-18 02:01:04 +0300

    JNI build 2026-02-18 02:01:04

---
+++ jni/jni/game/jniutil.cpp
@@ -0,0 +1,361 @@
+#include "jniutil.h"
+#include "chat.h"
+
+
+extern "C" JavaVM* javaVM;
+
+JNIEnv* CJavaWrapper::GetEnv()
+{
+    JNIEnv* env = nullptr;
+    int getEnvStat = javaVM->GetEnv((void**)& env, JNI_VERSION_1_6);
+
+    if (getEnvStat == JNI_EDETACHED)
+    {
+        if (javaVM->AttachCurrentThread(&env, NULL) != 0)
+        {
+            return nullptr;
+        }
+    }
+    if (getEnvStat == JNI_EVERSION)
+    {
+        return nullptr;
+    }
+
+    if (getEnvStat == JNI_ERR)
+    {
+        return nullptr;
+    }
+
+    return env;
+}
+#include "xorstr.h"
+
+void CJavaWrapper::ShowReg(const std::string &jsonString)
+{
+    JNIEnv* env = nullptr;
+    javaVM->GetEnv((void**)& env, JNI_VERSION_1_6);
+    if (!env) {
+        return;
+    }
+    if (guiManagerClass != NULL) {
+        jmethodID getInstanceMethod = env->GetStaticMethodID(guiManagerClass, "getInstance", "()Lcom/blackhub/bronline/game/GUIManager;");
+        if (getInstanceMethod != NULL) {
+            jobject guiManagerInstance = env->CallStaticObjectMethod(guiManagerClass, getInstanceMethod);
+
+            jstring jsonJString = env->NewStringUTF(jsonString.c_str()); // Преобразуем строку JSON в JNI тип
+
+            jmethodID showCurrentGUIMethod = env->GetMethodID(guiManagerClass, "onPacketIncoming", "(ILorg/json/JSONObject;)V");
+            if (showCurrentGUIMethod != NULL) {
+                jclass jsonObjectClass = env->FindClass("org/json/JSONObject");
+                if (jsonObjectClass != NULL) {
+                    jmethodID constructor = env->GetMethodID(jsonObjectClass, "<init>", "(Ljava/lang/String;)V");
+                    if (constructor != NULL) {
+                        jobject jsonObject = env->NewObject(jsonObjectClass, constructor, jsonJString);
+
+                        // Теперь вызываем метод Java
+                        env->CallVoidMethod(guiManagerInstance, showCurrentGUIMethod, 38 /* экран ID */, jsonObject);
+
+                        env->DeleteLocalRef(jsonObject);
+                    }
+                }
+            }
+
+            env->DeleteLocalRef(jsonJString);
+            env->DeleteLocalRef(guiManagerInstance);
+        }
+    }
+
+    env->DeleteLocalRef(guiManagerClass);
+
+    EXCEPTION_CHECK(env);
+}
+
+void CJavaWrapper::ShowReg2(const std::string &jsonString)
+{
+    JNIEnv* env = nullptr;
+    javaVM->GetEnv((void**)& env, JNI_VERSION_1_6);
+    if (!env) {
+        return;
+    }
+    if (guiManagerClass != NULL) {
+        jmethodID getInstanceMethod = env->GetStaticMethodID(guiManagerClass, "getInstance", "()Lcom/blackhub/bronline/game/GUIManager;");
+        if (getInstanceMethod != NULL) {
+            jobject guiManagerInstance = env->CallStaticObjectMethod(guiManagerClass, getInstanceMethod);
+
+            jstring jsonJString = env->NewStringUTF(jsonString.c_str()); // Преобразуем строку JSON в JNI тип
+
+            jmethodID showCurrentGUIMethod = env->GetMethodID(guiManagerClass, "handleFragmentScreen", "(ILorg/json/JSONObject;)V");
+            if (showCurrentGUIMethod != NULL) {
+                jclass jsonObjectClass = env->FindClass("org/json/JSONObject");
+                if (jsonObjectClass != NULL) {
+                    jmethodID constructor = env->GetMethodID(jsonObjectClass, "<init>", "(Ljava/lang/String;)V");
+                    if (constructor != NULL) {
+                        jobject jsonObject = env->NewObject(jsonObjectClass, constructor, jsonJString);
+
+                        // Теперь вызываем метод Java
+                        env->CallVoidMethod(guiManagerInstance, showCurrentGUIMethod, 38 /* экран ID */, jsonObject);
+
+                        env->DeleteLocalRef(jsonObject);
+                    }
+                }
+            }
+
+            env->DeleteLocalRef(jsonJString);
+            env->DeleteLocalRef(guiManagerInstance);
+        }
+    }
+
+    env->DeleteLocalRef(guiManagerClass);
+
+    EXCEPTION_CHECK(env);
+}
+void CJavaWrapper::ShowReward(const std::string &jsonString)
+{
+    JNIEnv* env = nullptr;
+    javaVM->GetEnv((void**)& env, JNI_VERSION_1_6);
+    if (!env) {
+        return;
+    }
+    if (guiManagerClass != NULL) {
+        jmethodID getInstanceMethod = env->GetStaticMethodID(guiManagerClass, "getInstance", "()Lcom/blackhub/bronline/game/GUIManager;");
+        if (getInstanceMethod != NULL) {
+            jobject guiManagerInstance = env->CallStaticObjectMethod(guiManagerClass, getInstanceMethod);
+
+            jstring jsonJString = env->NewStringUTF(jsonString.c_str()); // Преобразуем строку JSON в JNI тип
+
+            jmethodID showCurrentGUIMethod = env->GetMethodID(guiManagerClass, "onPacketIncoming", "(ILorg/json/JSONObject;)V");
+            if (showCurrentGUIMethod != NULL) {
+                jclass jsonObjectClass = env->FindClass("org/json/JSONObject");
+                if (jsonObjectClass != NULL) {
+                    jmethodID constructor = env->GetMethodID(jsonObjectClass, "<init>", "(Ljava/lang/String;)V");
+                    if (constructor != NULL) {
+                        jobject jsonObject = env->NewObject(jsonObjectClass, constructor, jsonJString);
+
+                        // Теперь вызываем метод Java
+                        env->CallVoidMethod(guiManagerInstance, showCurrentGUIMethod, 74 /* экран ID */, jsonObject);
+
+                        env->DeleteLocalRef(jsonObject);
+                    }
+                }
+            }
+
+            env->DeleteLocalRef(jsonJString);
+            env->DeleteLocalRef(guiManagerInstance);
+        }
+    }
+
+    env->DeleteLocalRef(guiManagerClass);
+
+    EXCEPTION_CHECK(env);
+}
+void CJavaWrapper::ShowCase(const std::string &jsonString)
+{
+    JNIEnv* env = nullptr;
+    javaVM->GetEnv((void**)& env, JNI_VERSION_1_6);
+    if (!env) {
+        return;
+    }
+    if (guiManagerClass != NULL) {
+        jmethodID getInstanceMethod = env->GetStaticMethodID(guiManagerClass, "getInstance", "()Lcom/blackhub/bronline/game/GUIManager;");
+        if (getInstanceMethod != NULL) {
+            jobject guiManagerInstance = env->CallStaticObjectMethod(guiManagerClass, getInstanceMethod);
+
+            jstring jsonJString = env->NewStringUTF(jsonString.c_str()); // Преобразуем строку JSON в JNI тип
+
+            jmethodID showCurrentGUIMethod = env->GetMethodID(guiManagerClass, "onPacketIncoming", "(ILorg/json/JSONObject;)V");
+            if (showCurrentGUIMethod != NULL) {
+                jclass jsonObjectClass = env->FindClass("org/json/JSONObject");
+                if (jsonObjectClass != NULL) {
+                    jmethodID constructor = env->GetMethodID(jsonObjectClass, "<init>", "(Ljava/lang/String;)V");
+                    if (constructor != NULL) {
+                        jobject jsonObject = env->NewObject(jsonObjectClass, constructor, jsonJString);
+
+                        // Теперь вызываем метод Java
+                        env->CallVoidMethod(guiManagerInstance, showCurrentGUIMethod, 73 /* экран ID */, jsonObject);
+
+                        env->DeleteLocalRef(jsonObject);
+                    }
+                }
+            }
+
+            env->DeleteLocalRef(jsonJString);
+            env->DeleteLocalRef(guiManagerInstance);
+        }
+    }
+
+    env->DeleteLocalRef(guiManagerClass);
+
+    EXCEPTION_CHECK(env);
+}
+void CJavaWrapper::ShowCase2(const std::string &jsonString)
+{
+    JNIEnv* env = nullptr;
+    javaVM->GetEnv((void**)& env, JNI_VERSION_1_6);
+    if (!env) {
+        return;
+    }
+    if (guiManagerClass != NULL) {
+        jmethodID getInstanceMethod = env->GetStaticMethodID(guiManagerClass, "getInstance", "()Lcom/blackhub/bronline/game/GUIManager;");
+        if (getInstanceMethod != NULL) {
+            jobject guiManagerInstance = env->CallStaticObjectMethod(guiManagerClass, getInstanceMethod);
+
+            jstring jsonJString = env->NewStringUTF(jsonString.c_str()); // Преобразуем строку JSON в JNI тип
+
+            jmethodID showCurrentGUIMethod = env->GetMethodID(guiManagerClass, "onPacketIncoming", "(ILorg/json/JSONObject;)V");
+            if (showCurrentGUIMethod != NULL) {
+                jclass jsonObjectClass = env->FindClass("org/json/JSONObject");
+                if (jsonObjectClass != NULL) {
+                    jmethodID constructor = env->GetMethodID(jsonObjectClass, "<init>", "(Ljava/lang/String;)V");
+                    if (constructor != NULL) {
+                        jobject jsonObject = env->NewObject(jsonObjectClass, constructor, jsonJString);
+
+                        // Теперь вызываем метод Java
+                        env->CallVoidMethod(guiManagerInstance, showCurrentGUIMethod, 66 /* экран ID */, jsonObject);
+
+                        env->DeleteLocalRef(jsonObject);
+                    }
+                }
+            }
+
+            env->DeleteLocalRef(jsonJString);
+            env->DeleteLocalRef(guiManagerInstance);
+        }
+    }
+
+    env->DeleteLocalRef(guiManagerClass);
+
+    EXCEPTION_CHECK(env);
+}
+void CJavaWrapper::ShowTuning(const std::string &jsonString)
+{
+    JNIEnv* env = nullptr;
+    javaVM->GetEnv((void**)& env, JNI_VERSION_1_6);
+    if (!env) {
+        return;
+    }
+    if (guiManagerClass != NULL) {
+        jmethodID getInstanceMethod = env->GetStaticMethodID(guiManagerClass, "getInstance", "()Lcom/blackhub/bronline/game/GUIManager;");
+        if (getInstanceMethod != NULL) {
+            jobject guiManagerInstance = env->CallStaticObjectMethod(guiManagerClass, getInstanceMethod);
+
+            jstring jsonJString = env->NewStringUTF(jsonString.c_str()); // Преобразуем строку JSON в JNI тип
+
+            jmethodID showCurrentGUIMethod = env->GetMethodID(guiManagerClass, "onPacketIncoming", "(ILorg/json/JSONObject;)V");
+            if (showCurrentGUIMethod != NULL) {
+                jclass jsonObjectClass = env->FindClass("org/json/JSONObject");
+                if (jsonObjectClass != NULL) {
+                    jmethodID constructor = env->GetMethodID(jsonObjectClass, "<init>", "(Ljava/lang/String;)V");
+                    if (constructor != NULL) {
+                        jobject jsonObject = env->NewObject(jsonObjectClass, constructor, jsonJString);
+
+                        // Теперь вызываем метод Java
+                        env->CallVoidMethod(guiManagerInstance, showCurrentGUIMethod, 27 /* экран ID */, jsonObject);
+
+                        env->DeleteLocalRef(jsonObject);
+                    }
+                }
+            }
+
+            env->DeleteLocalRef(jsonJString);
+            env->DeleteLocalRef(guiManagerInstance);
+        }
+    }
+
+    env->DeleteLocalRef(guiManagerClass);
+
+    EXCEPTION_CHECK(env);
+}
+void CJavaWrapper::ShowDonate(const std::string &jsonString)
+{
+    JNIEnv* env = nullptr;
+    javaVM->GetEnv((void**)& env, JNI_VERSION_1_6);
+    if (!env) {
+        return;
+    }
+    if (guiManagerClass != NULL) {
+        jmethodID getInstanceMethod = env->GetStaticMethodID(guiManagerClass, "getInstance", "()Lcom/blackhub/bronline/game/GUIManager;");
+        if (getInstanceMethod != NULL) {
+            jobject guiManagerInstance = env->CallStaticObjectMethod(guiManagerClass, getInstanceMethod);
+
+            jstring jsonJString = env->NewStringUTF(jsonString.c_str()); // Преобразуем строку JSON в JNI тип
+
+            jmethodID showCurrentGUIMethod = env->GetMethodID(guiManagerClass, "onPacketIncoming", "(ILorg/json/JSONObject;)V");
+            if (showCurrentGUIMethod != NULL) {
+                jclass jsonObjectClass = env->FindClass("org/json/JSONObject");
+                if (jsonObjectClass != NULL) {
+                    jmethodID constructor = env->GetMethodID(jsonObjectClass, "<init>", "(Ljava/lang/String;)V");
+                    if (constructor != NULL) {
+                        jobject jsonObject = env->NewObject(jsonObjectClass, constructor, jsonJString);
+
+                        // Теперь вызываем метод Java
+                        env->CallVoidMethod(guiManagerInstance, showCurrentGUIMethod, 22 /* экран ID */, jsonObject);
+
+                        env->DeleteLocalRef(jsonObject);
+                    }
+                }
+            }
+
+            env->DeleteLocalRef(jsonJString);
+            env->DeleteLocalRef(guiManagerInstance);
+        }
+    }
+
+    env->DeleteLocalRef(guiManagerClass);
+
+    EXCEPTION_CHECK(env);
+}
+void CJavaWrapper::ShowInv(const std::string &jsonString)
+{
+    JNIEnv* env = nullptr;
+    javaVM->GetEnv((void**)& env, JNI_VERSION_1_6);
+    if (!env) {
+        return;
+    }
+    if (guiManagerClass != NULL) {
+        jmethodID getInstanceMethod = env->GetStaticMethodID(guiManagerClass, "getInstance", "()Lcom/blackhub/bronline/game/GUIManager;");
+        if (getInstanceMethod != NULL) {
+            jobject guiManagerInstance = env->CallStaticObjectMethod(guiManagerClass, getInstanceMethod);
+
+            jstring jsonJString = env->NewStringUTF(jsonString.c_str()); // Преобразуем строку JSON в JNI тип
+
+            jmethodID showCurrentGUIMethod = env->GetMethodID(guiManagerClass, "onPacketIncoming", "(ILorg/json/JSONObject;)V");
+            if (showCurrentGUIMethod != NULL) {
+                jclass jsonObjectClass = env->FindClass("org/json/JSONObject");
+                if (jsonObjectClass != NULL) {
+                    jmethodID constructor = env->GetMethodID(jsonObjectClass, "<init>", "(Ljava/lang/String;)V");
+                    if (constructor != NULL) {
+                        jobject jsonObject = env->NewObject(jsonObjectClass, constructor, jsonJString);
+
+                        // Теперь вызываем метод Java
+                        env->CallVoidMethod(guiManagerInstance, showCurrentGUIMethod, 33 /* экран ID */, jsonObject);
+
+                        env->DeleteLocalRef(jsonObject);
+                    }
+                }
+            }
+
+            env->DeleteLocalRef(jsonJString);
+            env->DeleteLocalRef(guiManagerInstance);
+        }
+    }
+
+    env->DeleteLocalRef(guiManagerClass);
+
+    EXCEPTION_CHECK(env);
+}
+CJavaWrapper::CJavaWrapper(JNIEnv* env)
+{
+	guiManagerClass = env->FindClass("com/blackhub/bronline/game/GUIManager");
+	
+}
+
+CJavaWrapper::~CJavaWrapper()
+{
+    JNIEnv* pEnv = GetEnv();
+    if (pEnv)
+    {
+        pEnv->DeleteGlobalRef(this->activity);
+    }
+}
+
+CJavaWrapper* g_pJavaWrapper = nullptr;

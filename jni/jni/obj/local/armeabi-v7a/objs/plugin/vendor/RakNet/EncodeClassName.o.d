62097d5e8755c585200be9e86b58c7f0a440d618
root <root@vps1388.maze-host.ru>
2026-02-18 11:59:44 +0300

    Clean repository 2026-02-18 11:59:44

--- jni/jni/obj/local/armeabi-v7a/objs/plugin/vendor/RakNet/EncodeClassName.o.d
+++
@@ -1,2 +0,0 @@
-./obj/local/armeabi-v7a/objs/plugin/vendor/RakNet/EncodeClassName.o: \
-  jni/vendor/RakNet/EncodeClassName.cpp
--- jni/jni/obj/local/armeabi-v7a/objs/plugin/vendor/RakNet/ExtendedOverlappedPool.o.d
+++
@@ -1,2 +0,0 @@
-./obj/local/armeabi-v7a/objs/plugin/vendor/RakNet/ExtendedOverlappedPool.o: \
-  jni/vendor/RakNet/ExtendedOverlappedPool.cpp

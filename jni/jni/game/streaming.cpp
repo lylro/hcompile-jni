c34930b1286b1447c8546ede6796cfb04a334729
root <root@vps1388.maze-host.ru>
2026-02-22 17:32:36 +0300

    Clean repository 2026-02-22 17:32:36

--- jni/jni/game/streaming.cpp
+++
@@ -1,7 +0,0 @@
-#include "streaming.h"
-#include "../util/patch.h"
-
-int CStreaming::AddImageToList(const char * name, bool b)
-{
-    return CPatch::CallFunction<int>(g_libGTASA + 0x28E7B0 + 1, name, b);
-}
